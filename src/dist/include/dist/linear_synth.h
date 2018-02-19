e133fb57ddfb1afdb2a3904a7870d3c406989af4
Bruce Collie <bruce.collie@ed.ac.uk>
2018-02-19 10:20:57 +0000

    Start to fill out V2 interface

--- src/dist/include/dist/linear_synth.h
+++ src/dist/include/dist/linear_synth.h
@@ -29,19 +29,66 @@ namespace v2 {
 template <typename R, typename... Args>
 class Linear {
 public:
+  using ret_t = typename R::example_t;
+  using args_t = std::tuple<typename Args::example_t...>;
+  using io_pair_t = std::pair<ret_t, args_t>;
+
   Linear(R r, Args... args) :
     return_type_{r}, arg_types_{args...},
-    examples_{}
+    examples_{},
+    module_{std::make_unique<llvm::Module>("", ThreadContext::get())}
   {}
 
+  void add_example(ret_t ret, args_t args);
+
 private:
+  llvm::FunctionType *llvm_function_type() const;
+  size_t value_count(llvm::Function *f) const;
+  bool satisfies_examples(llvm::Function *f) const;
+  llvm::Value *sample(llvm::Function *f);
+
   R return_type_;
   std::tuple<Args...> arg_types_;
 
-  using example_t = std::pair<typename R::example_t, 
-                              std::tuple<typename Args::example_t...>>;
-  std::vector<example_t> examples_;
+  std::vector<io_pair_t> examples_;
+
+  std::unique_ptr<llvm::Module> module_;
 };
+
+template <typename R, typename... Args>
+void Linear<R, Args...>::add_example(Linear::ret_t r, Linear::args_t args)
+{
+  examples_.push_back({r, args});
+}
+
+template <typename R, typename... Args>
+llvm::FunctionType *Linear<R, Args...>::llvm_function_type() const
+{
+  auto llvm_arg_tys = std::array<llvm::Type*, sizeof...(Args)>{};
+  util::zip_for_each(arg_types_, llvm_arg_tys, [] (auto a, auto& ll) {
+    ll = a.llvm_type();
+  });
+  return llvm::FunctionType::get(return_type_.llvm_type(), llvm_arg_tys, false);
+}
+
+template <typename R, typename... Args>
+size_t Linear<R, Args...>::value_count(llvm::Function *f) const
+{
+  auto instrs = std::accumulate
+    (std::begin(*f), std::end(*f), 0, [f](auto sum, auto& bb) {
+      return sum + bb.size();
+    });
+  return instrs + f->arg_size();
+}
+
+template <typename R, typename... Args>
+bool Linear<R, Args...>::satisfies_examples(llvm::Function *f) const
+{
+  auto fc = FunctionCallable<R>(f);
+  return std::all_of(std::begin(examples_), std::end(examples_), [&fc](auto ex) {
+    return std::apply(fc, ex.second) == ex.first;
+  });
+}
 
 }
 
