c9568d8fd80fcbc8b99f5024debdabfce345fa85
Bruce Collie <brucecollie82@gmail.com>
2018-04-11 12:58:00 +0100

    Implement better synth metadata

--- src/dist/include/dist/synth_metadata.h
+++ src/dist/include/dist/synth_metadata.h
@@ -49,4 +49,93 @@ std::set<llvm::Value *> SynthMetadata::live_with(Pred&& p)
   return ret;
 }
 
+namespace v2 {
+
+template <typename Value, typename Key>
+class LookupRef {
+public:
+  LookupRef(std::map<Key, Value>& m, Key k) :
+    map_(m), k_(k) {}
+
+  operator Value*() const
+  {
+    if(auto d = map_.find(k_); d != std::end(map_)) {
+      return &(d->second);
+    } else {
+      return nullptr;
+    }
+  }
+
+  LookupRef<Value, Key>& operator=(Value v)
+  {
+    map_.insert_or_assign(k_, v);
+    return *this;
+  }
+
+private:
+  std::map<Key, Value>& map_;
+  Key k_;
+};
+
+template <typename Key>
+class LookupRef<bool, Key> {
+public:
+  LookupRef(std::set<Key>& m, Key k) :
+    items_(m), k_(k) {}
+
+  operator bool() const
+  {
+    auto d = items_.find(k_); 
+    return d != std::end(items_);
+  }
+
+  LookupRef<bool, Key>& operator=(bool v)
+  {
+    if(v) {
+      items_.insert(k_);
+    } else {
+      items_.erase(k_);
+    }
+    return *this;
+  }
+
+private:
+  std::set<Key>& items_;
+  Key k_;
+};
+
+template <typename Value, typename Key = llvm::Value *>
+class MetadataEntry {
+public:
+  auto operator()(Key k) { return LookupRef<Value, Key>(data_, k); }
+  void unset(Key k) { data_.erase(k); }
+
+  auto begin() { return std::begin(data_); }
+  auto end() { return std::end(data_); }
+
+private:
+  std::map<Key, Value> data_ = {};
+};
+
+template <typename Key>
+class MetadataEntry<bool, Key> {
+public:
+  auto operator()(Key k) { return LookupRef<bool, Key>(data_, k); }
+
+  auto begin() { return std::begin(data_); }
+  auto end() { return std::end(data_); }
+
+private:
+  std::set<Key> data_ = {};
+};
+
+struct SynthMetadata {
+  MetadataEntry<size_t> size;
+  MetadataEntry<size_t> index_bound;
+  MetadataEntry<bool>   oob;
+  MetadataEntry<bool>   live;
+};
+
+}
+
 }
