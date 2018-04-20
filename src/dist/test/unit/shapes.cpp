9c9a680fbe8eb628d7907c5386c998b55bd2ac39
Bruce Collie <brucecollie82@gmail.com>
2018-04-20 09:46:57 +0100

    Clear old version out

--- src/dist/test/unit/shapes.cpp
+++ src/dist/test/unit/shapes.cpp
@@ -8,15 +8,4 @@
 using namespace accsynt;
 
 TEST_CASE("testing loop shapes", "[shapes]") {
-  /* auto ids = std::vector<int>{0, 1, 2, 3, 4}; */
-  /* auto nested = make_nest( */
-  /*     Seq{ */
-  /*       Hole{}, */ 
-  /*       Seq{Hole{}, Hole{}} */
-  /*     } */
-  /* ); */
-
-  /* do { */
-  /*   std::cout << nested.instantiate(begin(ids)).first << '\n'; */
-  /* } while(std::next_permutation(begin(ids), begin(ids)+nested.size())); */
 }
