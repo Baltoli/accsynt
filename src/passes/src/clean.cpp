3d01f93948dfa15b6ab6466b119ebf5c1bdcd73d
Bruce Collie <brucecollie82@gmail.com>
2018-05-10 11:00:56 +0100

    Correctly remove error block

--- src/passes/src/clean.cpp
+++ src/passes/src/clean.cpp
@@ -1,6 +1,7 @@
 #include <llvm/Pass.h>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Function.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Support/raw_ostream.h>
 
@@ -14,10 +15,12 @@ struct Clean : public ModulePass {
   static char ID;
   Clean() : ModulePass(ID) {}
 
+  bool runOnModule(Module &M) override;
+
   BasicBlock* errorBlock(Function &F) const;
   bool hasErrorParameter(Function const &F) const;
 
-  bool runOnModule(Module &M) override;
+  void removeBlock(BasicBlock *BB) const;
 };
 
 bool Clean::runOnModule(Module &M)
@@ -26,6 +29,7 @@ bool Clean::runOnModule(Module &M)
     auto err_block = errorBlock(F);
 
     if(err_block && hasErrorParameter(F)) {
+      removeBlock(err_block);
       llvm::errs() << F.getName() << " to be cleaned!\n";
     }
   }
@@ -53,6 +57,29 @@ bool Clean::hasErrorParameter(Function const& F) const
   auto ptr_ty = llvm::PointerType::getUnqual(i64_ty);
 
   return F.arg_begin()->getType() == ptr_ty;
+}
+
+void Clean::removeBlock(BasicBlock *BB) const
+{
+  assert(BB && "Can't remove null basic block!");
+
+  for(auto user : BB->users()) {
+    auto branch = dyn_cast<BranchInst>(user);
+    assert(branch && "Basic block user is not a branch");
+
+    BasicBlock *other_dest = nullptr;
+    for(auto i = 0u; i < branch->getNumSuccessors(); ++i) {
+      auto succ = branch->getSuccessor(i);
+      if(succ != BB) {
+        other_dest = succ;
+        break;
+      }
+    }
+
+    auto parent = branch->getParent();
+    branch->eraseFromParent();
+    BranchInst::Create(other_dest, parent);
+  }
 }
 
 char Clean::ID = 0;
