d6b008367137bf5891046f065dc4a51fa43a0370
Bruce Collie <brucecollie82@gmail.com>
2018-10-01 10:33:18 +0100

    Start to write function to IDL conversion

---
+++ src/passes/src/to_idl.cpp
@@ -0,0 +1,26 @@
+#include <llvm/Pass.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+
+namespace {
+
+struct ConvertToIDL : public FunctionPass {
+  static char ID;
+  ConvertToIDL() : FunctionPass(ID) {}
+
+  bool runOnFunction(Function& F) override;
+};
+
+bool ConvertToIDL::runOnFunction(Function& F)
+{
+  return false;
+}
+
+char ConvertToIDL::ID = 0;
+static RegisterPass<ConvertToIDL> X("to_idl", "Convert a function to IDL constraints",
+                                    false, false);
+
+}
