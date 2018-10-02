359ccd898b5e9390e7cd271492cc689463cf4174
Bruce Collie <brucecollie82@gmail.com>
2018-10-02 11:33:14 +0100

    Start to build convert tool

--- src/tools/src/convert.cpp
+++ src/tools/src/convert.cpp
@@ -1,5 +1,22 @@
 #include <passes/passes.h>
 
-int main()
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/SourceMgr.h>
+
+using namespace llvm;
+
+static cl::opt<std::string>
+InputFilename(cl::Positional, cl::desc("<input bitcode file>"),
+    cl::init("-"), cl::value_desc("filename"));
+
+int main(int argc, char **argv)
 {
+  cl::ParseCommandLineOptions(argc, argv);
+  LLVMContext Context;
+  SMDiagnostic Err;
+
+  auto&& mod = parseIRFile(InputFilename, Err, Context, true, "");
 }
