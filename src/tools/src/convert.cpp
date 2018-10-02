3c4093fee32783be0577521e92d649df0091b012
Bruce Collie <brucecollie82@gmail.com>
2018-10-02 12:00:37 +0100

    Conversion options

--- src/tools/src/convert.cpp
+++ src/tools/src/convert.cpp
@@ -9,8 +9,18 @@
 using namespace llvm;
 
 static cl::opt<std::string>
+FunctionName(cl::Positional, cl::desc("<function to convert to IDL>"),
+    cl::value_desc("function name"));
+
+static cl::opt<std::string>
 InputFilename(cl::Positional, cl::desc("<input bitcode file>"),
     cl::init("-"), cl::value_desc("filename"));
+
+static cl::opt<std::string>
+OutputFilename("o",
+    cl::desc("Filename to save the generated constraints to"),
+    cl::value_desc("filename"),
+    cl::init("-"));
 
 int main(int argc, char **argv)
 {
@@ -19,4 +29,12 @@ int main(int argc, char **argv)
   SMDiagnostic Err;
 
   auto&& mod = parseIRFile(InputFilename, Err, Context, true, "");
+  if(!mod) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
+
+  if(OutputFilename == "-") {
+    OutputFilename = FunctionName + ".idl";
+  }
 }
