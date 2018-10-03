6a7d9b89b0ebc954187a59c045c104b0f358bbde
Bruce Collie <brucecollie82@gmail.com>
2018-10-03 10:00:53 +0100

    Very basic add implementation

--- src/passes/src/translate.cpp
+++ src/passes/src/translate.cpp
@@ -7,15 +7,45 @@
 
 using namespace llvm;
 
+std::string nth_of(int i)
+{
+  if(i == 0) { return "first"; }
+  else if(i == 1) { return "second"; }
+  else if(i == 2) { return "third"; }
+  else {
+    assert(false && "Too many operands");
+  }
+}
+
 std::optional<std::string> constraint(llvm::Instruction const& I)
 {
+  auto operands = I.getNumOperands();
+  if(operands > 3) {
+    return std::nullopt;
+  }
+
   if(auto op = idl_opcode(I)) {
-    return fmt::format(
-      "( {{{result}}} is {op} instruction and {{{left}}} is first argument of {{{result}}} )",
+    auto args = std::vector<std::string>{};
+    for(auto i = 0u; i < operands; ++i) {
+      args.push_back(fmt::format(
+        "{{{arg}}} is {nth} argument of {{{result}}}",
+        fmt::arg("arg", I.getOperand(i)->getName().str()),
+        fmt::arg("nth", nth_of(i)),
+        fmt::arg("result", I.getName().str())
+      ));
+    }
+
+    auto op_str = fmt::format(
+      "{{{result}}} is {op} instruction", 
       fmt::arg("result", I.getName().str()),
-      fmt::arg("op", op.value()),
-      fmt::arg("left", "left")
-    );             
+      fmt::arg("op", op.value())
+    );
+
+    if(args.empty()) {
+      return fmt::format("( {} )", op_str);
+    } else {
+      return fmt::format("( {} and ( {} ) )", op_str, fmt::join(args, " and "));
+    }
   }
 
   return std::nullopt;
