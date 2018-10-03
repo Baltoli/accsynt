c95ced779fb023b385da14f43cc5333b8ce8ec34
Bruce Collie <brucecollie82@gmail.com>
2018-10-03 15:18:33 +0100

    Implement detection tool

--- src/tools/src/detect.cpp
+++ src/tools/src/detect.cpp
@@ -4,18 +4,27 @@
 #include <llvm/PassRegistry.h>
 #include <llvm/PassSupport.h>
 #include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/PluginLoader.h>
+#include <llvm/Support/SourceMgr.h>
 
 using namespace llvm;
 
+static cl::opt<std::string>
+InputFilename(cl::Positional, cl::desc("<input bitcode file>"),
+    cl::init("-"), cl::value_desc("filename"));
+
 struct pass_finder : public PassRegistrationListener {
-  pass_finder(std::string pn) :
+  pass_finder(std::string pn, PassRegistry& reg) :
     pass_name(pn)
   {
+    reg.enumerateWith(this);
   }
 
-  void passEnumerate(PassInfo const* pi)
+  void passEnumerate(PassInfo const* pi) override
   {
     if(pi->getPassArgument() == pass_name) {
       pass_info = pi;
@@ -33,13 +42,26 @@ int main(int argc, char **argv)
   cl::ParseCommandLineOptions(argc, argv);
 
   auto& reg = *PassRegistry::getPassRegistry();
-  auto d = pass_finder{"detect"};
+  auto d = pass_finder{"detect", reg};
 
-  reg.enumerateWith(&d);
-  if(d.pass_info) {
-    auto impl = d.pass_info->createPass();
-    auto pm = legacy::PassManager{};
-
-    pm.add(impl);
+  if(!d.pass_info) {
+    fmt::print(stderr, 
+        "Could not load detection pass - make sure it is loaded with -load\n");
+    return 1;
   }
+
+  auto impl = d.pass_info->createPass();
+  auto pm = legacy::PassManager{};
+  pm.add(impl);
+
+  LLVMContext Context;
+  SMDiagnostic Err;
+
+  auto&& mod = parseIRFile(InputFilename, Err, Context, true, "");
+  if(!mod) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
+
+  pm.run(*mod);
 }
