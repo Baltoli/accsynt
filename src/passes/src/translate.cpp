60dd3168f986578889925ecf996d642f587e7622
Bruce Collie <brucecollie82@gmail.com>
2018-10-04 10:42:55 +0100

    Better rewrite of instruction translation

--- src/passes/src/translate.cpp
+++ src/passes/src/translate.cpp
@@ -7,7 +7,7 @@
 
 using namespace llvm;
 
-std::string nth_of(int i)
+std::string nth_of(size_t i)
 {
   if(i == 0) { return "first"; }
   else if(i == 1) { return "second"; }
@@ -17,35 +17,69 @@ std::string nth_of(int i)
   }
 }
 
-std::optional<std::string> constraint(llvm::Instruction const& I)
+// Base constraint for an instruction - i.e. {I} is add instruction
+std::optional<std::string> base_constraint(Instruction const& I)
 {
-  auto operands = I.getNumOperands();
-  if(operands > 3) {
-    return std::nullopt;
-  }
+  using namespace fmt::literals;
 
   if(auto op = idl_opcode(I)) {
-    auto args = std::vector<std::string>{};
-    for(auto i = 0u; i < operands; ++i) {
-      args.push_back(fmt::format(
-        "{{{arg}}} is {nth} argument of {{{result}}}",
-        fmt::arg("arg", I.getOperand(i)->getName().str()),
-        fmt::arg("nth", nth_of(i)),
-        fmt::arg("result", I.getName().str())
-      ));
-    }
-
-    auto op_str = fmt::format(
-      "{{{result}}} is {op} instruction", 
-      fmt::arg("result", I.getName().str()),
-      fmt::arg("op", op.value())
+    return "({{{result}}} is {op} instruction)"_format(
+      "result"_a = I.getName().str(),
+      "op"_a = op.value()
     );
+  }
 
-    if(args.empty()) {
-      return fmt::format("( {} )", op_str);
-    } else {
-      return fmt::format("( {} and ( {} ) )", op_str, fmt::join(args, " and "));
+  return std::nullopt;
+}
+
+// Constraint for the nth argument of I
+// i.e. {v0} is first argument of {I}
+std::string nth_arg_constraint(Instruction const& I, size_t n)
+{
+  using namespace fmt::literals;
+  assert(n < I.getNumOperands() && 
+         "Not enough operands to instruction");
+
+  auto nth_text = nth_of(n);
+  return "({{{arg}}} is {nth} argument of {{{instr}}})"_format(
+      "arg"_a = I.getOperand(n)->getName().str(),
+      "nth"_a = nth_text,
+      "instr"_a = I.getName().str()
+  );
+}
+
+// Combine constraints into one by wrapping them in braces and forming their
+// conjunction
+template <typename Iterator>
+std::string constraint_and(Iterator begin, Iterator end)
+{
+  using namespace fmt::literals;
+
+  return "({})"_format(fmt::join(begin, end, " and "));
+}
+
+// Comvenience overload
+template <typename Container>
+std::string constraint_and(Container c)
+{
+  using std::begin;
+  using std::end;
+
+  return constraint_and(begin(c), end(c));
+}
+
+std::optional<std::string> constraint(Instruction const& I)
+{
+  auto operands = I.getNumOperands();
+
+  if(auto op_str = base_constraint(I); operands <= 3) {
+    auto args = std::vector<std::string>{op_str.value()};
+
+    for(auto n = 0u; n < operands; ++n) {
+      args.push_back(nth_arg_constraint(I, n));
     }
+
+    return constraint_and(args);
   }
 
   return std::nullopt;
