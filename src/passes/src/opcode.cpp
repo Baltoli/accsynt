669eb30a52deb96279c060f0b9bdfe65961a45a8
Bruce Collie <brucecollie82@gmail.com>
2018-10-04 12:21:30 +0100

    More opcodes

--- src/passes/src/opcode.cpp
+++ src/passes/src/opcode.cpp
@@ -2,13 +2,32 @@
 
 using namespace llvm;
 
-bool is_add(Instruction const& inst)
-{
-  return inst.getOpcode() == Instruction::Add;
-}
-
 std::optional<std::string> idl_opcode(llvm::Instruction const& inst)
 {
-  if(is_add(inst)) { return "add"; }
-  return std::nullopt;
+  switch(inst.getOpcode()) {
+    case Instruction::GetElementPtr: 
+      return "gep";
+    case Instruction::Shl:
+      return "lshift";
+    case Instruction::LShr:
+    case Instruction::AShr:
+      return "rshift";
+    case Instruction::Add:
+    case Instruction::FAdd:
+    case Instruction::Sub:
+    case Instruction::FSub:
+    case Instruction::Mul:
+    case Instruction::FMul:
+    case Instruction::UDiv:
+    case Instruction::SDiv:
+    case Instruction::FDiv:
+    case Instruction::PHI:
+    case Instruction::And:
+    case Instruction::Or:
+    case Instruction::ICmp:
+    case Instruction::FCmp:
+      return inst.getOpcodeName(inst.getOpcode());
+    default: 
+      return std::nullopt;
+  }
 }
