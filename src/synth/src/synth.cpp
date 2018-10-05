ce9e5fc4db68a5899b01b01e8f5f98631040f5b0
Bruce Collie <brucecollie82@gmail.com>
2018-10-05 16:29:47 +0100

    Start to load properties

--- src/synth/src/synth.cpp
+++ src/synth/src/synth.cpp
@@ -1,3 +1,27 @@
-int main()
+#include <props/props.h>
+#include <support/dynamic_library.h>
+
+#include <fmt/format.h>
+
+#include <llvm/Support/CommandLine.h>
+
+using namespace llvm;
+
+static cl::opt<std::string>
+PropertiesPath(
+    cl::Positional, cl::Required,
+    cl::desc("<properties file>"));
+
+static cl::opt<std::string>
+LibraryPath(
+    cl::Positional, cl::Required,
+    cl::desc("<shared library>"));
+
+// In the future, specifications...
+
+int main(int argc, char **argv)
 {
+  cl::ParseCommandLineOptions(argc, argv);
+
+  auto property_set = props::property_set::load(PropertiesPath);
 }
