ef97b6784017fc8d6e2fc1fe9155e54945e4efe5
Bruce Collie <brucecollie82@gmail.com>
2018-10-08 15:57:47 +0100

    Convert params to llvm types

--- src/props/src/llvm_function.cpp
+++ src/props/src/llvm_function.cpp
@@ -1,17 +1,27 @@
 #include <props/props.h>
+#include <support/thread_context.h>
 
+using namespace support;
 using namespace llvm;
 
 namespace props {
 
 Type *param::llvm_type() const
 {
-  auto base_type = [this] {
-    return nullptr;
+  auto base_type = [this] () -> Type* {
+    switch(type) {
+      case data_type::integer:
+        return IntegerType::get(thread_context::get(), 32);
+      case data_type::floating:
+        return Type::getFloatTy(thread_context::get());
+      default:
+        assert(false && "Unsupported data type");
+    }
   }();
 
   Type *full_type = base_type;
   for(auto i = 0; i < pointer_depth; ++i) {
+    full_type = PointerType::getUnqual(full_type);
   }
 
   return full_type;
