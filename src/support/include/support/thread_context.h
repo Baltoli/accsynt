041d63ab302bdd7a60d54fefe63d2bc038dea26e
Bruce Collie <brucecollie82@gmail.com>
2018-10-08 15:49:28 +0100

    Thread contexts now in support lib

---
+++ src/support/include/support/thread_context.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <llvm/IR/LLVMContext.h>
+
+#include <unordered_map>
+#include <memory>
+#include <mutex>
+#include <thread>
+
+namespace support {
+
+class thread_context {
+public:
+  thread_context(const thread_context&) = delete;
+  void operator=(const thread_context&) = delete;
+
+  static llvm::LLVMContext& get();
+  static llvm::LLVMContext& get(std::thread::id id);
+  static llvm::LLVMContext& get(const std::thread& t);
+
+private:
+  thread_context() : 
+    mapping_{}
+  {}
+
+  static thread_context& instance();
+
+  std::unordered_map<
+    std::thread::id, 
+    std::unique_ptr<llvm::LLVMContext>
+  > mapping_;
+
+  std::mutex map_mutex_;
+};
+
+}
---
+++ src/support/src/thread_context.cpp
@@ -0,0 +1,36 @@
+#include <support/thread_context.h>
+
+using namespace llvm;
+
+namespace support {
+
+thread_context& thread_context::instance()
+{
+  static thread_context instance;
+  return instance;
+}
+
+LLVMContext& thread_context::get(std::thread::id id)
+{
+  std::lock_guard l{instance().map_mutex_};
+
+  auto& m = instance().mapping_;
+
+  if(m.find(id) == std::end(m)) {
+    m[id] = std::make_unique<LLVMContext>();
+  }
+
+  return *m[id];
+}
+
+LLVMContext& thread_context::get()
+{
+  return get(std::this_thread::get_id());
+}
+
+LLVMContext& thread_context::get(const std::thread& t)
+{
+  return get(t.get_id());
+}
+
+}
