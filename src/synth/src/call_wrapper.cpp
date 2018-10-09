3fcdabefa54d8e30744027484a920383d4abe7bc
Bruce Collie <brucecollie82@gmail.com>
2018-10-09 16:56:35 +0100

    Start to implement call wrapper

--- src/synth/src/call_wrapper.cpp
+++ src/synth/src/call_wrapper.cpp
@@ -1,4 +1,18 @@
 #include "call_wrapper.h"
 
+#include <support/llvm_cloning.h>
+#include <support/thread_context.h>
+
+using namespace support;
+using namespace llvm;
+
 namespace synth {
+
+call_wrapper::call_wrapper(Module& mod, std::string const& name)
+{
+  auto mod_copy = copy_module_to(thread_context::get(), &mod);
+  auto eb = llvm::EngineBuilder{std::move(mod_copy)};
+  engine_.reset(eb.create());
+}
+
 }
--- src/synth/src/call_wrapper.h
+++ src/synth/src/call_wrapper.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/Interpreter.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Module.h>
+
+#include <memory>
+
 namespace synth {
 
 class call_wrapper {
 public:
+  call_wrapper(llvm::Module& mod, std::string const& name);
+
 private:
+  std::unique_ptr<llvm::ExecutionEngine> engine_;
 };
 
 }
