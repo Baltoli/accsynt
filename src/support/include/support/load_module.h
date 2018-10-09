f28da35081ee74ca03c0a867cff70dfa7e234ae0
Bruce Collie <brucecollie82@gmail.com>
2018-10-09 12:43:33 +0100

    Parse test modules

---
+++ src/support/include/support/load_module.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#pragma once
+
+#include <llvm/IR/Module.h>
+
+#include <memory>
+
+#define PARSE_TEST_MODULE(name, str) \
+  auto name = support::parse_module(str); \
+  REQUIRE(mod);
+
+namespace support {
+
+std::unique_ptr<llvm::Module> parse_module(const std::string& str);
+
+}
---
+++ src/support/src/load_module.cpp
@@ -0,0 +1,29 @@
+#include <support/load_module.h>
+#include <support/thread_context.h>
+
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/SourceMgr.h>
+
+using namespace llvm;
+
+namespace support {
+
+std::unique_ptr<llvm::Module> parse_module(const std::string& str)
+{
+  auto sm = SMDiagnostic{};
+  auto buf = MemoryBuffer::getMemBuffer(str);
+  auto mod = parseIR(*buf, sm, thread_context::get());
+
+  if(!mod) {
+    sm.print(nullptr, llvm::errs());
+    std::exit(1);
+  }
+
+  llvm::verifyModule(*mod, &llvm::errs());
+  return mod;
+}
+
+}
