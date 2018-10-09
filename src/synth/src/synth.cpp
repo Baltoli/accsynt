02d5a785e71702c6b1e453356d352f69a495ad88
Bruce Collie <brucecollie82@gmail.com>
2018-10-09 14:14:12 +0100

    Fix shared compilation

--- src/synth/src/synth.cpp
+++ src/synth/src/synth.cpp
@@ -5,6 +5,7 @@
 
 #include <llvm/Support/CommandLine.h>
 
+using namespace support;
 using namespace llvm;
 
 static cl::opt<std::string>
@@ -24,5 +25,8 @@ int main(int argc, char **argv)
   cl::ParseCommandLineOptions(argc, argv);
 
   auto property_set = props::property_set::load(PropertiesPath);
-  // also load shared library
+  auto fn_name = property_set.type_signature.name;
+
+  auto lib = dynamic_library(LibraryPath);
+  auto sym = lib.raw_symbol(fn_name);
 }
