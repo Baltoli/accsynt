26a56fb0ab662c9103257f49cbf104f6b8f4b3dd
Bruce Collie <brucecollie82@gmail.com>
2018-10-10 11:49:01 +0100

    Add signature to call wrapper

--- src/synth/src/call_wrapper.cpp
+++ src/synth/src/call_wrapper.cpp
@@ -3,28 +3,33 @@
 #include <support/llvm_cloning.h>
 #include <support/thread_context.h>
 
+using namespace props;
 using namespace support;
+
 using namespace llvm;
 
 namespace synth {
 
-call_wrapper::call_wrapper(Module const& mod)
+call_wrapper::call_wrapper(signature sig, Module const& mod)
+  : signature_(sig)
 {
   auto mod_copy = copy_module_to(thread_context::get(), mod);
   auto eb = llvm::EngineBuilder{std::move(mod_copy)};
   engine_.reset(eb.create());
 }
 
-call_wrapper::call_wrapper(llvm::Module const& mod, 
+call_wrapper::call_wrapper(signature sig, 
+                           llvm::Module const& mod, 
                            std::string const& name)
-  : call_wrapper(mod)
+  : call_wrapper(sig, mod)
 {
 }
 
-call_wrapper::call_wrapper(llvm::Module const& mod, 
+call_wrapper::call_wrapper(signature sig, 
+                           llvm::Module const& mod, 
                            std::string const& name, 
                            dynamic_library const& dl)
-  : call_wrapper(mod)
+  : call_wrapper(sig, mod)
 {
 }
 
--- src/synth/src/call_wrapper.h
+++ src/synth/src/call_wrapper.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <props/props.h>
 #include <support/dynamic_library.h>
 
 #include <llvm/ExecutionEngine/GenericValue.h>
@@ -13,12 +14,13 @@ namespace synth {
 
 class call_wrapper {
 public:
-  call_wrapper(llvm::Module const& mod, std::string const& name);
-  call_wrapper(llvm::Module const& mod, std::string const& name, support::dynamic_library const& dl);
+  call_wrapper(props::signature sig, llvm::Module const& mod, std::string const& name);
+  call_wrapper(props::signature sig, llvm::Module const& mod, std::string const& name, support::dynamic_library const& dl);
 
 private:
-  call_wrapper(llvm::Module const& mod);
+  call_wrapper(props::signature sig, llvm::Module const& mod);
 
+  props::signature signature_;
   llvm::Function *function_;
   std::unique_ptr<llvm::ExecutionEngine> engine_;
 };
