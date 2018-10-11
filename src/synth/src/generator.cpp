d3f5e1cf477f823fae0afd68c086fa4025ea439f
Bruce Collie <brucecollie82@gmail.com>
2018-10-11 13:30:11 +0100

    Generator build

---
+++ src/synth/src/generator.cpp
@@ -0,0 +1,4 @@
+#include "generator.h"
+
+namespace synth {
+}
---
+++ src/synth/src/generator.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "call_builder.h"
+
+#include <props/props.h>
+
+namespace synth {
+
+class generator {
+public:
+private:
+};
+
+}
