e2ed0f18b6ad8aca8ad9095cb122a2c3980ef47e
Bruce Collie <brucecollie82@gmail.com>
2018-10-11 14:24:43 +0100

    More thought about generators

--- src/synth/src/generator.cpp
+++ src/synth/src/generator.cpp
@@ -1,4 +1,16 @@
 #include "generator.h"
 
+using namespace props;
+
 namespace synth {
+
+generator::generator(property_set ps) :
+  properties_(ps)
+{
+}
+
+void generator::generate(call_builder& builder)
+{
+}
+
 }
--- src/synth/src/generator.h
+++ src/synth/src/generator.h
@@ -6,9 +6,20 @@
 
 namespace synth {
 
+/*
+ * The initial generator implementation is just going to randomly generate the
+ * values for the signature. However, the interface will accept a property_set
+ * rather than just a signature so that the properties can be used during the
+ * generation process (i.e. respecting fixed values, zeroing outputs etc).
+ */
 class generator {
 public:
-private:
+  generator(props::property_set ps);
+
+  virtual void generate(call_builder& builder);
+
+protected:
+  props::property_set properties_;
 };
 
 }
