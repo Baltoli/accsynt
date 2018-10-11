26cf18b412c9d50f58890d1dbf8d041dc871c1ee
Bruce Collie <brucecollie82@gmail.com>
2018-10-11 15:55:05 +0100

    Validations

--- src/synth/src/call_builder.h
+++ src/synth/src/call_builder.h
@@ -39,6 +39,7 @@ private:
   props::signature signature_;
   std::vector<uint8_t> args_;
 
+  size_t current_arg_ = 0;
   std::vector<std::vector<int>> int_data_ = {};
   std::vector<std::vector<float>> float_data_ = {};
 };
@@ -52,9 +53,18 @@ void call_builder::add(T arg)
                 !std::is_pointer_v<Base>,
                 "Must be int or float and not pointer!");
 
+  assert(current_arg_ < signature_.parameters.size());
+
+  auto param = signature_.parameters.at(current_arg_);
+  assert((std::is_same_v<Base, int> == (param.type == props::data_type::integer)));
+  assert((std::is_same_v<Base, float> == (param.type == props::data_type::floating)));
+  assert(param.pointer_depth == 0);
+
   for(auto i = 0u; i < sizeof(T); ++i) {
     args_.push_back(detail::nth_byte(arg, i));
   }
+
+  current_arg_++;
 }
 
 template <typename T>
@@ -64,8 +74,9 @@ void call_builder::add(std::vector<T> arg)
                 std::is_same_v<T, float>,
                 "Pointed-to data must be of base type");
 
-  void *data = nullptr;
+  assert(current_arg_ < signature_.parameters.size());
 
+  void *data = nullptr;
   if constexpr(std::is_same_v<T, int>) {
     int_data_.push_back(arg);
     data = int_data_.back().data();
@@ -80,6 +91,8 @@ void call_builder::add(std::vector<T> arg)
   for(auto i = 0u; i < sizeof(T); ++i) {
     args_.push_back(detail::nth_byte(data, i));
   }
+
+  current_arg_++;
 }
 
 }
