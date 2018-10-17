6a9c72ab12dee09ea63f59a4712f363a272e2149
Bruce Collie <brucecollie82@gmail.com>
2018-10-17 15:14:12 +0100

    Start to add loop shapes to BLAS synth

---
+++ src/support/include/support/visitor.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace support {
+
+template<class... Ts>
+struct visitor: Ts... { 
+  using Ts::operator()...; 
+};
+
+template<class... Ts> visitor(Ts...) -> visitor<Ts...>;
+
+}
