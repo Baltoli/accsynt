5dadcf1a46ccceb019bd799456eb12924bc86505
Bruce Collie <brucecollie82@gmail.com>
2018-10-25 16:09:19 +0100

    Build empty fix floats pass

---
+++ src/passes/src/fix_floats.cpp
@@ -0,0 +1,28 @@
+#include <passes/passes.h>
+
+#include <llvm/Pass.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+
+namespace {
+
+struct FixFloats : public FunctionPass {
+  static char ID;
+
+  FixFloats() : 
+    FunctionPass(ID) {}
+
+  bool runOnFunction(Function& f) override;
+};
+
+bool FixFloats::runOnFunction(Function& f) {
+  return false;
+}
+
+char FixFloats::ID = 0;
+static RegisterPass<FixFloats> X("fix-floats", "Do some unsafe floating point optimisations",
+                                 false, false);
+
+}
