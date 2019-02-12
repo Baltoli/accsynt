ec3417ab78762ce688ad2052a3604cde69434cfa
Bruce Collie <brucecollie82@gmail.com>
2019-02-12 16:40:31 +0000

    Fleshing out region find interface

--- src/interfind/include/interfind/region.h
+++ src/interfind/include/interfind/region.h
@@ -1,4 +1,53 @@
 #pragma once
 
+#include <vector>
+
+namespace llvm {
+  class Function;
+  class Type;
+  class Value;
+}
+
 namespace interfind {
+
+/**
+ * Represents a separable region of code that can be extracted into a function.
+ */
+class region {
+public:
+  /**
+   * Direct constructor from the values comprising this region.
+   */
+  region(llvm::Value *, std::vector<llvm::Value *>);
+
+private:
+  llvm::Value *output_;
+  std::vector<llvm::Value *> inputs_;
+};
+
+/**
+ * Responsible for searching a function for candidate regions with the given
+ * type.
+ */
+class region_finder {
+public:
+  /**
+   * Construct a region with the given return and argument types, as well as a
+   * function to search in.
+   */
+  region_finder(llvm::Function &, llvm::Type *, std::vector<llvm::Type *>);
+
+  // TODO: should really be unordered_set
+  /**
+   * Compute all the possible combinations of instructions that could represent
+   * regions in the given function.
+   */
+  std::vector<region> all_candidates() const;
+
+private:
+  llvm::Function& function_;
+  llvm::Type *return_type_;
+  std::vector<llvm::Type *> argument_types_;
+};
+
 }
--- src/interfind/src/region.cpp
+++ src/interfind/src/region.cpp
@@ -1,4 +1,30 @@
 #include <interfind/region.h>
 
+#include <llvm/IR/Dominators.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Value.h>
+
+using namespace llvm;
+
 namespace interfind {
+
+region::region(Value *out, std::vector<Value *> in) :
+  output_(out), inputs_(in)
+{
+}
+
+
+region_finder::region_finder(Function& fn, Type *out_t, 
+                             std::vector<Type *> in_ts) :
+  function_(fn), return_type_(out_t), argument_types_(in_ts)
+{
+}
+
+std::vector<region> region_finder::all_candidates() const
+{
+  auto dom_tree = DominatorTree(function_);
+  return {};
+}
+
 }
