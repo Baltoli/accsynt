b9bddb0f7e3bd313d49acbfab8c33bda232e9f55
Bruce Collie <brucecollie82@gmail.com>
2019-02-13 11:48:33 +0000

    Refactor to predicate visitor

--- src/interfind/include/interfind/visitors.h
+++ src/interfind/include/interfind/visitors.h
@@ -1,21 +1,24 @@
 #pragma once
 
+#include <llvm/IR/Constant.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instruction.h>
 #include <llvm/IR/InstVisitor.h>
+#include <llvm/IR/Value.h>
 
 #include <set>
 
-namespace llvm {
-  class Function;
-  class Instruction;
-  class Type;
-  class Value;
-}
-
 namespace interfind {
 
-class type_collect_visitor : public llvm::InstVisitor<type_collect_visitor> {
+/**
+ * Collect all the values that satisfy pred.
+ */
+namespace detail {
+
+template <typename Pred>
+class predicate_visitor : public llvm::InstVisitor<predicate_visitor<Pred>> {
 public:
-  type_collect_visitor(llvm::Type *);
+  predicate_visitor(Pred);
   std::set<llvm::Value *> values() const;
 
   void visitInstruction(llvm::Instruction& inst);
@@ -24,18 +27,61 @@ public:
   void visitValues(Range&&);
 
 private:
-  llvm::Type *type_;
+  Pred pred_;
   std::set<llvm::Value *> vals_;
 };
 
+template <typename Pred>
+predicate_visitor<Pred>::predicate_visitor(Pred p) :
+  pred_(p), vals_{}
+{
+}
+
+template <typename Pred>
+std::set<llvm::Value *> predicate_visitor<Pred>::values() const
+{
+  return vals_;
+}
+
+template <typename Pred>
+void predicate_visitor<Pred>::visitInstruction(llvm::Instruction& inst)
+{
+  if(pred_(inst)) {
+    vals_.insert(&inst);
+  }
+
+  for(auto& op : inst.operands()) {
+    if(auto* cst = llvm::dyn_cast<llvm::Constant>(&op)) {
+      if(pred_(*cst)) {
+        vals_.insert(cst);
+      }
+    }
+  }
+}
+
+template <typename Pred>
 template <typename Range>
-void type_collect_visitor::visitValues(Range&& rng)
+void predicate_visitor<Pred>::visitValues(Range&& rng)
 {
   for(auto& val : rng) {
-    if(val.getType() == type_) {
+    if(pred_(val)) {
       vals_.insert(&val);
     }
   }
+}
+
+template <typename Pred>
+std::set<llvm::Value *> values_by_pred(llvm::Function& fn, Pred pred)
+{
+  auto vis = detail::predicate_visitor(pred);
+
+  vis.visit(fn);
+  vis.visitValues(fn.getParent()->globals());
+  vis.visitValues(fn.args());
+
+  return vis.values();
+}
+
 }
 
 std::set<llvm::Value *> values_of_type(llvm::Function&, llvm::Type *);
--- src/interfind/src/visitors.cpp
+++ src/interfind/src/visitors.cpp
@@ -7,40 +7,11 @@ using namespace llvm;
 
 namespace interfind {
 
-type_collect_visitor::type_collect_visitor(Type *ty) :
-  type_(ty), vals_()
-{
-}
-
-std::set<Value *> type_collect_visitor::values() const
-{
-  return vals_;
-}
-
-void type_collect_visitor::visitInstruction(llvm::Instruction& inst)
-{
-  if(inst.getType() == type_) {
-    vals_.insert(&inst);
-  }
-
-  for(auto& op : inst.operands()) {
-    if(auto* cst = dyn_cast<Constant>(&op)) {
-      if(cst->getType() == type_) {
-        vals_.insert(cst);
-      }
-    }
-  }
-}
-
 std::set<Value *> values_of_type(Function &fn, Type *ty)
 {
-  auto vis = type_collect_visitor(ty);
-
-  vis.visit(fn);
-  vis.visitValues(fn.getParent()->globals());
-  vis.visitValues(fn.args());
-
-  return vis.values();
+  return detail::values_by_pred(fn, [ty] (auto& val) {
+    return val.getType() == ty;
+  });
 }
 
 }
