da37309ec4e8a0f0dbefda87dc21043511f5d3aa
Bruce Collie <brucecollie82@gmail.com>
2019-02-14 12:01:49 +0000

    Start to build use-def tests

--- src/interfind/test/use_def.cpp
+++ src/interfind/test/use_def.cpp
@@ -9,8 +9,21 @@ using namespace support;
 using namespace interfind;
 using namespace llvm;
 
+#define VALUE(name) \
+  auto name = get_by_name(fn, #name); \
+  REQUIRE(name);
+
 TEST_CASE("can compute use-def analyses", "[use_def]")
 {
-  auto mod = load_module(RESOURCE_DIR "adds.bc");
-  auto fn = mod->getFunction("adds");
+  auto mod = load_module(RESOURCE_DIR "deps.bc");
+  auto& fn = *mod->begin();
+
+  auto analysis = use_def_analysis(fn);
+
+  VALUE(arg0); VALUE(arg1); VALUE(arg2);
+  VALUE(v0); VALUE(v1); VALUE(v2);
+  VALUE(v3); VALUE(v4); VALUE(v5);
+
+  REQUIRE(analysis.depends(v0, arg0));
+  REQUIRE(analysis.depends(v0, arg1));
 }
