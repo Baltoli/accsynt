9c59f04ddb04180e7bc9c15a02c8081bda3f2f86
Bruce Collie <brucecollie82@gmail.com>
2019-02-14 10:58:19 +0000

    Add use-def analysis interface

--- src/interfind/include/interfind/use_def.h
+++ src/interfind/include/interfind/use_def.h
@@ -1,9 +1,72 @@
 #pragma once
 
+#include <map>
+#include <set>
+
+namespace llvm {
+  class Function;
+  class Instruction;
+  class Value;
+}
+
 namespace interfind {
 
-// one to find out if a value depends on another
+/**
+ * Computes use-def analysis queries for a function - for example, does an
+ * instruction appear in another's use-def chain?
+ */
+class use_def_analysis {
+public:
+  /**
+   * Maps instructions onto the set of values on which they depend (i.e. every value
+   * that appears in the SSA DAG with the key value as a leaf).
+   *
+   * The reason that the key type is Instruction rather than value is that only
+   * instructions depend on other values - the functions wrapping accesses to
+   * this type handle function arguments, global values etc.
+   */
+  using use_def_map = std::map<llvm::Instruction *, std::set<llvm::Value *>>;
 
-// one to find out if a value has "excess dependencies"
+  /**
+   * Default constructor does not compute the map - left to the user to do so by
+   * explicitly calling recompute.
+   */
+  use_def_analysis();
+
+  /**
+   * Construct the analysis for the given function, computing the map of
+   * instructions to dependencies.
+   */
+  use_def_analysis(llvm::Function&);
+
+  /**
+   * Perform analysis again, discarding previous results.
+   */
+  void recompute(llvm::Function&);
+
+  /**
+   * Does the value `use` depend on the value `def`?
+   */
+  bool depends(llvm::Value *use, llvm::Value *def) const;
+
+  /**
+   * Is it the case that the dependency graph of `use` can be rooted at the
+   * values contained in `roots` without any improper cutting of edges? That is,
+   * can `use` be expressed purely in terms of the values in `roots`?
+   */
+  bool is_root_set(llvm::Value *use, std::set<llvm::Value *> roots) const;
+
+private:
+  /**
+   * Internal map of dependencies.
+   */
+  use_def_map deps_;
+
+  /**
+   * Function over which this analysis is being computed. Used to check for
+   * validity when queries are made.
+   */
+  llvm::Function *function_;
+};
 
 }
