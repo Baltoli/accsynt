a8ca7688e8af17560ecfe44a56f2cb1334943b91
Bruce Collie <brucecollie82@gmail.com>
2019-02-14 16:45:42 +0000

    Implement generic find

--- src/support/include/support/traits.h
+++ src/support/include/support/traits.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 #include <type_traits>
 
@@ -23,6 +24,36 @@ struct lazy_false : std::false_type {};
 
 template <typename T>
 constexpr inline bool lazy_false_v = lazy_false<T>::value;
+
+template <typename, typename = std::void_t<>>
+struct has_member_find : std::false_type {};
+
+template <typename T>
+struct has_member_find<T,
+  std::void_t<decltype(
+    std::declval<T>().find(
+      std::declval<typename T::key_type>()
+    )
+  )>
+> : std::true_type {};
+
+template <typename T>
+constexpr inline bool has_member_find_v = has_member_find<T>::value;
+
+template <typename Container, typename Key>
+decltype(auto) container_find(Container&& c, Key const& key) 
+{
+  using std::begin;
+  using std::end;
+
+  if constexpr(has_member_find_v<Container>) {
+    return std::forward<decltype(c)>(c).find(key);
+  } else {
+    return std::find(begin(std::forward<decltype(c)>(c)), 
+                     end(std::forward<decltype(c)>(c)), 
+                     key);
+  }
+}
 
 }
 
--- src/support/test/traits.cpp
+++ src/support/test/traits.cpp
@@ -2,6 +2,9 @@
 
 #include <catch2/catch.hpp>
 
+#include <set>
+#include <map>
+
 using namespace support;
 
 struct s {};
@@ -22,5 +25,42 @@ TEST_CASE("pointer depth calculations work") {
     REQUIRE(pointer_depth_v<void***> == 3);
     REQUIRE(pointer_depth_v<s****> == 4);
     REQUIRE(pointer_depth_v<t*****> == 5);
+  }
+}
+
+struct has_find {
+  using key_type = int;
+  int find(key_type);
+};
+
+TEST_CASE("has_member_find works") {
+  SECTION("trait works correctly") {
+    static_assert(has_member_find_v<std::set<int>>,
+                  "Set should have member find");
+
+    static_assert(!has_member_find_v<std::vector<std::string>>,
+                  "Vector should not have member find");
+
+    static_assert(has_member_find_v<has_find>,
+                  "Types that look OK should also");
+  }
+
+  SECTION("generic find works") {
+    auto set = std::set{1, 2, 3};
+    auto vec = std::vector{7, 5, 3, 1};
+
+    auto s_found = container_find(set, 2);
+    REQUIRE(s_found != set.end());
+    REQUIRE(*s_found == 2);
+
+    auto v_found = container_find(vec, 1);
+    REQUIRE(v_found != vec.end());
+    REQUIRE(*v_found == 1);
+
+    auto s_not = container_find(set, 4);
+    REQUIRE(s_not == set.end());
+
+    auto v_not = container_find(vec, 4);
+    REQUIRE(v_not == vec.end());
   }
 }
