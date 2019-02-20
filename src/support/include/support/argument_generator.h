96c0401365c1d34be6b3e4b6a1f281d13f2ac47e
Bruce Collie <brucecollie82@gmail.com>
2019-02-20 17:14:06 +0000

    Start to test new generator

--- src/support/include/support/argument_generator.h
+++ src/support/include/support/argument_generator.h
@@ -1,4 +1,61 @@
 #pragma once
+ 
+#include <limits>
+#include <memory>
 
 namespace support {
+
+class argument_generator {
+public:
+  template <typename T>
+  argument_generator(T&& strat) :
+    strategy_(std::make_unique<model<T>>(strat))
+  {
+  }
+
+  // Interface
+  int gen_int(int min = std::numeric_limits<int>::min(),
+              int max = std::numeric_limits<int>::max())
+  {
+    return strategy_->gen_int(min, max);
+  }
+
+  float gen_float(float min = std::numeric_limits<float>::min(),
+                  float max = std::numeric_limits<float>::max())
+  {
+    return strategy_->gen_float(min, max);
+  }
+
+  // Type erasure
+  struct concept {
+    virtual ~concept() {}
+    virtual int gen_int(int min, int max) = 0;
+    virtual float gen_float(float min, float max) = 0;
+  };
+
+  template <typename T>
+  struct model : concept {
+    model(T& obj) :
+      object_(obj)
+    {
+    }
+
+    int gen_int(int min, int max) override
+    {
+      return object_.gen_int(min, max);
+    }
+
+    float gen_float(float min, float max) override
+    {
+      return object_.gen_float(min, max);
+    }
+
+  private:
+    T& object_;
+  };
+
+private:
+  std::unique_ptr<concept> strategy_;
+};
+
 }
--- src/support/test/argument_generator.cpp
+++ src/support/test/argument_generator.cpp
@@ -1,1 +1,14 @@
 #include <catch2/catch.hpp>
+
+#include <support/argument_generator.h>
+
+using namespace support;
+
+struct gen {
+  int gen_int(int min, int max) { return 2; }
+  float gen_float(float min, float max) { return 3; }
+};
+
+TEST_CASE("type erasure works") {
+  auto g = argument_generator(gen());
+}
