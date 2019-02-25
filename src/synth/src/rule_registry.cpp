93efd7e7166b877e34f7212077f91d1429677510
Bruce Collie <brucecollie82@gmail.com>
2019-02-25 12:22:18 +0000

    Unformat rule registry

--- src/synth/src/rule_registry.cpp
+++ src/synth/src/rule_registry.cpp
@@ -6,24 +6,44 @@ std::vector<rule> rule_registry::all()
 {
   using match = match_expression;
 
-  return { { "regularLoop", { "sz", "ptr" }, { match("size", "ptr", "sz") },
-               { negation("output", "ptr") } },
+  // clang-format off
+  return { 
+    { "regularLoop", { "sz", "ptr" }, 
+      { match("size", "ptr", "sz") },
+      { negation("output", "ptr") } 
+    },
     { "outputLoop", { "sz", "ptr" },
-        { match("size", "ptr", "sz"), match("output", "ptr") }, {} },
+      { match("size", "ptr", "sz"), 
+        match("output", "ptr") }, 
+      {} 
+    },
     { "regularLoop", { "sz", "ptrA", "ptrB" },
-        { match("size", "ptrA", "sz"), match("size", "ptrB", "sz") },
-        { distinct("ptrA", "ptrB"), negation("output", "ptrA") } },
+      { match("size", "ptrA", "sz"), 
+        match("size", "ptrB", "sz") },
+      { distinct("ptrA", "ptrB"), 
+        negation("output", "ptrA") } 
+    },
     { "outputLoop", { "sz", "ptrA", "ptrB" },
-        { match("size", "ptrA", "sz"), match("size", "ptrB", "sz"),
-            match("output", "ptrA") },
-        { distinct("ptrA", "ptrB") } },
+      { match("size", "ptrA", "sz"), 
+        match("size", "ptrB", "sz"),
+        match("output", "ptrA") },
+      { distinct("ptrA", "ptrB") } 
+    },
     { "regularLoop", { "sz", "ptrA", "ptrB", "ptrC" },
-        { match("size", "ptrA", "sz"), match("size", "ptrB", "sz"),
-            match("size", "ptrC", "sz") },
-        { distinct("ptrA", "ptrB", "ptrC"), negation("output", "ptrA") } },
+      { match("size", "ptrA", "sz"), 
+        match("size", "ptrB", "sz"),
+        match("size", "ptrC", "sz") },
+      { distinct("ptrA", "ptrB", "ptrC"), 
+        negation("output", "ptrA") } 
+    },
     { "outputLoop", { "sz", "ptrA", "ptrB", "ptrC" },
-        { match("size", "ptrA", "sz"), match("size", "ptrB", "sz"),
-            match("size", "ptrC", "sz"), match("output", "ptrA") },
-        { distinct("ptrA", "ptrB", "ptrC") } } };
+      { match("size", "ptrA", "sz"), 
+        match("size", "ptrB", "sz"),
+        match("size", "ptrC", "sz"), 
+        match("output", "ptrA") },
+      { distinct("ptrA", "ptrB", "ptrC") } 
+    } 
+  };
+  // clang-format on
 }
 }
