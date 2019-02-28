0b9cbbb812669b315758a6fea6099ac0f43a6871
Bruce Collie <brucecollie82@gmail.com>
2019-02-28 11:54:40 +0000

    Start to implement generator rules

--- src/synth/src/generator_rules.cpp
+++ src/synth/src/generator_rules.cpp
@@ -1,1 +1,26 @@
 #include "generator_rules.h"
+
+using namespace props;
+using namespace support;
+
+namespace synth {
+
+argument_generator generator_for(property_set ps)
+{
+  for (auto prop : ps.properties) {
+    if (prop.name == "generator") {
+    }
+  }
+
+  return generator_named("uniform");
+}
+
+argument_generator generator_named(std::string const& name)
+{
+  if (name == "uniform") {
+    return uniform_generator();
+  }
+
+  throw std::runtime_error("No such named generator");
+}
+}
