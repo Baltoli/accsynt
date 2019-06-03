d888dab3f45e1de7279914e8461f58acdcfa6985
Bruce Collie <brucecollie82@gmail.com>
2019-06-03 15:10:31 +0100

    Start to define distance metric

--- src/support/include/support/similarity.h
+++ src/support/include/support/similarity.h
@@ -1,4 +1,20 @@
 #pragma once
 
+#include <support/call_builder.h>
+
 namespace support {
-}
+
+/**
+ * Compute the similarity between the two call builders (before or after they
+ * have been passed to a call_wrapper and potentially modified).
+ *
+ * The design of this metric is currently as follows:
+ *  - A score of 0.0 indicates that the two are totally identical (i.e. they
+ *    compare equal using operator==).
+ *  - If the two have different type signatures, then the two are the "least
+ *    similar" they possibly could be, so return infinity.
+ *  - Otherwise, the following rules are used to define similarity:
+ */
+double similarity(call_builder const& a, call_builder const& b);
+
+} // namespace support
--- src/support/src/similarity.cpp
+++ src/support/src/similarity.cpp
@@ -1,4 +1,18 @@
 #include <support/similarity.h>
 
+#include <numeric_limits>
+
 namespace support {
+
+double similarity(call_builder const& a, call_builder const& b)
+{
+  if (a.signature() != b.signature()) {
+    return std::numeric_limits<double>::infinity();
+  } else if (a == b) {
+    return 0.0;
+  }
+
+  return 1.0;
 }
+
+} // namespace support
