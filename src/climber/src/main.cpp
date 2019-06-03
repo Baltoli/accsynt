06ed9b7ef262ee3ad5b0d010c0c19c4f124e6478
Bruce Collie <brucecollie82@gmail.com>
2019-06-03 14:38:57 +0100

    Allow for regular function pointers to be called

--- src/climber/src/main.cpp
+++ src/climber/src/main.cpp
@@ -1,14 +1,42 @@
 #include <fmt/format.h>
 
+#include <llvm/IR/Module.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+
 #include <props/props.h>
 #include <support/call_wrapper.h>
+#include <support/thread_context.h>
 
-int func(int x, int y) { return x + y; }
+using namespace props;
+using namespace support;
+using namespace llvm;
+
+int func(int x, int y) { return x * y; }
+int func_2(int n, int* vs)
+{
+  int sum = 0;
+  for (int i = 0; i < n; ++i) {
+    sum += vs[i];
+  }
+  return sum;
+}
 
 int main()
 {
   using namespace props::literals;
-  auto sig = "int func(int x, int y)"_sig;
 
-  /* auto wrap = support::call_wrapper(sig, func); */
+  InitializeNativeTarget();
+  LLVMInitializeNativeAsmPrinter();
+  LLVMInitializeNativeAsmParser();
+
+  auto sig = "int func(int x, int *ys)"_sig;
+  auto mod = Module("test_mod", thread_context::get());
+  auto wrap = call_wrapper(sig, mod, "func", func_2);
+
+  auto build = wrap.get_builder();
+  build.add(5, std::vector{ 1, 2, 3, 4, 5 });
+
+  auto ret = wrap.call(build);
+  fmt::print("{}\n", ret);
 }
