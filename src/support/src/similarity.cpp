63133f5aedf7e648a7c8aa1c849353e9b0fe80c9
Bruce Collie <brucecollie82@gmail.com>
2019-06-04 12:05:29 +0100

    Dispatch to correct return metric

--- src/support/src/similarity.cpp
+++ src/support/src/similarity.cpp
@@ -1,10 +1,12 @@
+#include <props/props.h>
 #include <support/similarity.h>
 
 #include <cmath>
 
 namespace support {
 
-double return_similarity(uint64_t ret_a, uint64_t ret_b) { return 0.0; }
+double int_return_similarity(uint64_t ret_a, uint64_t ret_b) { return 0.0; }
+double float_return_similarity(uint64_t ret_a, uint64_t ret_b) { return 0.0; }
 
 double params_similarity(call_builder const& a, call_builder const& b)
 {
@@ -20,13 +22,26 @@ double similarity(uint64_t ret_a, call_builder const& a, uint64_t ret_b,
     return 1.0;
   }
 
-  constexpr auto return_weight = 0.5;
-  constexpr auto params_weight = (1 - return_weight);
+  auto return_weight = 0.5;
+  auto return_comp = 0.0;
+  auto params_weight = 0.5;
 
-  auto return_comp = return_weight * return_similarity(ret_a, ret_b);
-  auto params_comp = params_weight * params_similarity(a, b);
+  auto const& sig = a.signature();
+  if (auto rt_opt = sig.return_type) {
+    if (*rt_opt == props::data_type::integer) {
+      return_comp = int_return_similarity(ret_a, ret_b);
+    } else if (*rt_opt == props::data_type::floating) {
+      return_comp = float_return_similarity(ret_a, ret_b);
+    } else {
+      throw std::runtime_error("Invalid return type for metric");
+    }
+  } else {
+    return_weight = 0.0;
+    params_weight = 1.0;
+  }
 
-  return return_comp + params_comp;
+  auto params_comp = params_similarity(a, b);
+  return return_weight * return_comp + params_weight * params_comp;
 }
 
 } // namespace support
