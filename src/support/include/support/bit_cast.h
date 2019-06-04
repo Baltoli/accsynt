39d2fd54a1de0d9efc91d1d1940cadfa1207a167
Bruce Collie <brucecollie82@gmail.com>
2019-06-04 11:22:25 +0100

    Loosen restrictions

--- src/support/include/support/bit_cast.h
+++ src/support/include/support/bit_cast.h
@@ -5,14 +5,13 @@
 namespace support {
 
 template <class To, class From>
-typename std::enable_if<(sizeof(To) == sizeof(From))
-        && std::is_trivially_copyable<From>::value
+typename std::enable_if<std::is_trivially_copyable<From>::value
         && std::is_trivial<To>::value,
     To>::type
 bit_cast(const From& src) noexcept
 {
   To dst;
-  std::memcpy(&dst, &src, sizeof(To));
+  std::memcpy(&dst, &src + (sizeof(From) - sizeof(To)), sizeof(To));
   return dst;
 }
 
