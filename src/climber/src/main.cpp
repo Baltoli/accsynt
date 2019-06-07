11ec2f8775264a7dcface143a3544ba690ca8b24
Bruce Collie <brucecollie82@gmail.com>
2019-06-07 10:49:12 +0100

    Has NaNs

--- src/climber/src/main.cpp
+++ src/climber/src/main.cpp
@@ -6,16 +6,26 @@
 
 #include <props/props.h>
 #include <support/call_wrapper.h>
+#include <support/similarity.h>
 #include <support/thread_context.h>
 
 using namespace props;
 using namespace support;
 using namespace llvm;
 
-int map_mul(int n, float v, float* xs)
+void map_mul(int n, float v, float* xs)
 {
   for (int i = 0; i < n; ++i) {
     xs[i] *= v;
+  }
+}
+
+void map_add(int n, float v, float* xs)
+{
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      xs[i] *= xs[j];
+    }
   }
 }
 
@@ -27,13 +37,23 @@ int main()
   LLVMInitializeNativeAsmPrinter();
   LLVMInitializeNativeAsmParser();
 
-  auto sig = "int func(int x, int *ys)"_sig;
+  auto sig = "void func(int n, float v, float *xs)"_sig;
   auto mod = Module("test_mod", thread_context::get());
-  auto wrap = call_wrapper(sig, mod, "func", func_2);
 
-  auto build = wrap.get_builder();
-  build.add(5, std::vector{ 1, 2, 3, 4, 5 });
+  auto mul = call_wrapper(sig, mod, "map_mul", map_mul);
+  auto add = call_wrapper(sig, mod, "map_add", map_add);
 
-  auto ret = wrap.call(build);
-  fmt::print("{}\n", ret);
+  auto m_b = mul.get_builder();
+  auto a_b = add.get_builder();
+
+  m_b.add(5, 2.0f, std::vector<float>{ 1, 2, 3, 4, 5 });
+  a_b.add(5, 2.0f, std::vector<float>{ 1, 2, 3, 4, 5 });
+
+  auto r_m = mul.call(m_b);
+  auto r_a = add.call(a_b);
+
+  auto sim = similarity(r_m, m_b, r_a, a_b);
+  fmt::print("{}\n", sim);
+
+  /* auto ret = wrap.call(build); */
 }
