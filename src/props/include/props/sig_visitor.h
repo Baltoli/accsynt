3a42f6310998e79a5db5d4a0b820ebd91a311d35
Bruce Collie <brucecollie82@gmail.com>
2019-06-10 17:19:36 +0100

    Implement and test new visitor

--- src/props/include/props/sig_visitor.h
+++ src/props/include/props/sig_visitor.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <props/core.h>
+#include <support/tuple.h>
 
+#include <optional>
 #include <tuple>
 
 namespace props {
@@ -140,13 +142,73 @@ extern ignore_param_t ignore_param;
 
 namespace v2 {
 
-template <typename Func>
-struct on {
+template <typename F>
+class on {
+public:
+  on(data_type dt, F&& f);
+  on(data_type dt, int depth, F&& f);
+
+  void operator()(param const& p);
+
+private:
+  data_type dt_;
+  std::optional<int> depth_;
+  F f_;
 };
 
-template <typename... Ons>
-struct sig_visitor {
+template <typename F>
+on<F>::on(data_type dt, F&& f)
+    : dt_(dt)
+    , depth_()
+    , f_(std::forward<F>(f))
+{
+}
+
+template <typename F>
+on<F>::on(data_type dt, int depth, F&& f)
+    : dt_(dt)
+    , depth_(depth)
+    , f_(std::forward<F>(f))
+{
+}
+
+template <typename F>
+void on<F>::operator()(param const& p)
+{
+  if (dt_ == p.type) {
+    if (depth_) {
+      if (*depth_ == p.pointer_depth) {
+        f_(p);
+      }
+    } else if (p.pointer_depth == 0) {
+      f_(p);
+    }
+  }
+}
+
+template <typename... Fs>
+class sig_visitor {
+public:
+  sig_visitor(Fs&&... fns);
+  void visit(signature const& sig);
+
+private:
+  std::tuple<Fs...> fns_;
 };
+
+template <typename... Fs>
+sig_visitor<Fs...>::sig_visitor(Fs&&... fns)
+    : fns_(std::forward<Fs>(fns)...)
+{
+}
+
+template <typename... Fs>
+void sig_visitor<Fs...>::visit(signature const& sig)
+{
+  for (auto const& param : sig.parameters) {
+    support::for_each(fns_, [&](auto& fn) { fn(param); });
+  }
+}
 
 } // namespace v2
 
--- src/props/test/visitor.cpp
+++ src/props/test/visitor.cpp
@@ -130,6 +130,19 @@ TEST_CASE("can visit type signatures")
     REQUIRE_NOTHROW(v4.visit(s4));
   }
 }
-// clang-format on
 
-TEST_CASE("Can use the v2 visitor") {}
+TEST_CASE("Can use the v2 visitor")
+{
+  SECTION("in a very basic case") {
+    auto s = "void f(float a, int *b, int c)"_sig;
+
+    auto c = 0;
+    auto vis = v2::sig_visitor{
+      v2::on(data_type::integer, [&] (auto const&) { ++c; })
+    };
+
+    vis.visit(s);
+    REQUIRE(c == 1);
+  }
+}
+// clang-format on
