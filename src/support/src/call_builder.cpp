ccf8a1671583972a3a9246e2866a7c3d3b5bca27
Bruce Collie <brucecollie82@gmail.com>
2019-06-12 12:27:37 +0100

    Conversion of more code

--- src/support/src/call_builder.cpp
+++ src/support/src/call_builder.cpp
@@ -13,22 +13,21 @@ call_builder::call_builder(props::signature sig)
 {
 }
 
-signature const& call_builder::signature() const
-{
-  return signature_;
-}
+signature const& call_builder::signature() const { return signature_; }
 
 call_builder::call_builder(call_builder const& other)
     : signature_(other.signature_)
     , current_arg_(0)
     , int_data_(other.int_data_)
     , float_data_(other.float_data_)
+    , char_data_(other.char_data_)
 {
   args_.clear();
 
   size_t offset = 0;
   size_t int_offset = 0;
   size_t float_offset = 0;
+  size_t char_offset = 0;
 
   for (auto const& param : signature_.parameters) {
     if (current_arg_ >= other.current_arg_) {
@@ -37,7 +36,7 @@ call_builder::call_builder(call_builder const& other)
 
     if (param.pointer_depth == 0) {
       // TODO: fix size for more types
-      for (auto i = 0u; i < 4; ++i, ++offset) {
+      for (auto i = 0u; i < data_type_size(param.type); ++i, ++offset) {
         args_.push_back(other.args_.at(offset));
       }
     } else {
@@ -46,6 +45,8 @@ call_builder::call_builder(call_builder const& other)
       void* data = nullptr;
       if (param.type == data_type::integer) {
         data = int_data_.at(int_offset++).data();
+      } else if (param.type == data_type::character) {
+        data = char_data_.at(char_offset++).data();
       } else if (param.type == data_type::floating) {
         data = float_data_.at(float_offset++).data();
       }
@@ -61,10 +62,7 @@ call_builder::call_builder(call_builder const& other)
   }
 }
 
-void call_builder::reset()
-{
-  *this = call_builder(signature_);
-}
+void call_builder::reset() { *this = call_builder(signature_); }
 
 call_builder& call_builder::operator=(call_builder other)
 {
@@ -80,6 +78,7 @@ void swap(call_builder& left, call_builder& right)
   swap(left.current_arg_, right.current_arg_);
   swap(left.int_data_, right.int_data_);
   swap(left.float_data_, right.float_data_);
+  swap(left.char_data_, right.char_data_);
 }
 
 uint8_t* call_builder::args()
@@ -91,8 +90,8 @@ uint8_t* call_builder::args()
 
 bool call_builder::operator==(call_builder const& other) const
 {
-  // Easy up front checks - we need to be storing the same quantity of argument
-  // data, and we need the same signature.
+  // Easy up front checks - we need to be storing the same quantity of
+  // argument data, and we need the same signature.
   if (args_.size() != other.args_.size()) {
     return false;
   }
@@ -109,6 +108,7 @@ bool call_builder::operator==(call_builder const& other) const
   // parameter of the appropriate data type.
   size_t int_data_offset = 0;
   size_t float_data_offset = 0;
+  size_t char_data_offset = 0;
 
   bool all_eq = true;
 
@@ -119,14 +119,13 @@ bool call_builder::operator==(call_builder const& other) const
 
     if (param.pointer_depth == 0) {
       // Value comparisons done on the raw byte data
-      // TODO: fix hard-coded size assumption when I have more types
-      for (auto i = 0u; i < 4; ++i, ++offset) {
+      for (auto i = 0u; i < data_type_size(param.type); ++i, ++offset) {
         all_eq = all_eq && (args_.at(offset) == other.args_.at(offset));
       }
     } else {
-      // Pointer comparisons need to look at the data stored, and need to ignore
-      // 8 bytes of the stored argument bytes as well - pointers will be
-      // different
+      // Pointer comparisons need to look at the data stored, and need to
+      // ignore 8 bytes of the stored argument bytes as well - pointers will
+      // be different
       offset += 8;
 
       if (param.type == data_type::integer) {
@@ -134,6 +133,11 @@ bool call_builder::operator==(call_builder const& other) const
             && (int_data_.at(int_data_offset)
                    == other.int_data_.at(int_data_offset));
         int_data_offset++;
+      } else if (param.type == data_type::character) {
+        all_eq = all_eq
+            && (char_data_.at(char_data_offset)
+                   == other.char_data_.at(char_data_offset));
+        char_data_offset++;
       } else if (param.type == data_type::floating) {
         all_eq = all_eq
             && approx_equal(float_data_.at(float_data_offset),
@@ -150,4 +154,4 @@ bool call_builder::operator!=(call_builder const& other) const
 {
   return !(*this == other);
 }
-}
+} // namespace support
