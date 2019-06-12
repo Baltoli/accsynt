9ee3b006eea28d26444cfb6c94f007aaa1ae7aca
Bruce Collie <brucecollie82@gmail.com>
2019-06-12 12:15:17 +0100

    Final call builder tests

--- src/support/test/call_builder.cpp
+++ src/support/test/call_builder.cpp
@@ -233,32 +233,37 @@ TEST_CASE("Can extract arguments from a call_builder")
 
   SECTION("Can get whole signatures back correctly")
   {
-    auto c1 = call_builder("int fun(int a, int *b, float *c, float d)"_sig);
+    auto c1
+        = call_builder("int fun(int a, int *b, float *c, float d, char e)"_sig);
     auto a = 784123;
     auto b = std::vector{ 1, 2, 3 };
     auto c = std::vector{ 0.4f, 0.2f, 453.2f };
     auto d = 5768.11f;
-    c1.add(a, b, c, d);
+    auto e = '{';
+    c1.add(a, b, c, d, e);
 
     REQUIRE(c1.get<int>(0) == a);
     REQUIRE(c1.get<std::vector<int>>(1) == b);
     REQUIRE(c1.get<std::vector<float>>(2) == c);
     REQUIRE(c1.get<float>(3) == d);
+    REQUIRE(c1.get<char>(4) == e);
   }
 
   SECTION("Can get parameters back by name")
   {
     auto c1 = call_builder(
-        "int fun(int *aio_efd, int woo, float *c__a, float doo)"_sig);
+        "int fun(int *aio_efd, int woo, float *c__a, float doo, char eep)"_sig);
     auto aio_efd = std::vector{ 1, 2, 3 };
     auto woo = 784123;
     auto c__a = std::vector{ 0.4f, 0.2f, 453.2f };
     auto doo = 5768.11f;
-    c1.add(aio_efd, woo, c__a, doo);
+    auto eep = '{';
+    c1.add(aio_efd, woo, c__a, doo, eep);
 
     REQUIRE(c1.get<std::vector<int>>("aio_efd") == aio_efd);
     REQUIRE(c1.get<int>("woo") == woo);
     REQUIRE(c1.get<std::vector<float>>("c__a") == c__a);
     REQUIRE(c1.get<float>("doo") == doo);
+    REQUIRE(c1.get<char>("eep") == eep);
   }
 }
