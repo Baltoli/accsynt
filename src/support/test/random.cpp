bcb7a568cd4f547bc44984bf870c425ab1e472b2
Bruce Collie <brucecollie82@gmail.com>
2019-06-20 10:15:59 +0100

    Test for empty sample

--- src/support/test/random.cpp
+++ src/support/test/random.cpp
@@ -19,3 +19,9 @@ TEST_CASE("Can sample from tuples using auto lambdas")
   auto str = ss.str();
   REQUIRE(((str == "1") || (str == "3.45") || (str == "ir")));
 }
+
+TEST_CASE("Sampling from empty works")
+{
+  auto t = std::tuple{};
+  REQUIRE_NOTHROW(uniform_tuple_sample(t, [](auto) { throw 0; }));
+}
