92bdd5dfcc907e96e8e7a8289081acafa2904cba
Bruce Collie <brucecollie82@gmail.com>
2019-07-31 10:29:30 +0100

    Stop GEMV segfaults

--- src/synth/test/inputs/gemv.c
+++ src/synth/test/inputs/gemv.c
@@ -1,9 +1,12 @@
-void gemv(int M, int N, float *A, float *x, float *y)
+void gemv(int M, int N, float* A, float* x, float* y)
 {
-  for(int i = 0; i < M; ++i) {
+  for (int i = 0; i < M; ++i) {
     float sum = 0;
-    for(int j = 0; j < N; ++j) {
-      sum += A[j + i*N] * x[j];
+    for (int j = 0; j < N; ++j) {
+      float add = A[j + i * N];
+      add *= x[j];
+      sum += add;
+      /* sum += A[j + i*N] * x[j]; */
     }
     y[i] = sum;
   }
