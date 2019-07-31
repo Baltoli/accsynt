574fa17865a886b8a0387b46dfb582e2bbf1a792
Bruce Collie <brucecollie82@gmail.com>
2019-07-31 12:07:29 +0100

    Some more implementation

--- src/synth/src/affine_fragment.cpp
+++ src/synth/src/affine_fragment.cpp
@@ -14,6 +14,30 @@ affine_fragment::affine_fragment(std::vector<props::value> args)
 {
 }
 
+void affine_fragment::splice(
+    compile_context& ctx, llvm::BasicBlock* entry, llvm::BasicBlock* exit)
+{
+  // TODO
+}
+
+bool affine_fragment::add_child(frag_ptr f, size_t idx)
+{
+  auto max = count_or_empty(before_);
+  if (idx < max) {
+    if (before_) {
+      before_->add_child(f, idx);
+    } else {
+      before_ = f;
+    }
+
+    return true;
+  } else {
+    idx -= max;
+  }
+
+  throw std::invalid_argument("Too few holes in fragment");
+}
+
 std::string affine_fragment::to_str(size_t indent)
 {
   return "AFFINE"; // TODO
