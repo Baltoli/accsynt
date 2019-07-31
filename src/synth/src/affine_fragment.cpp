b02c7c95494f4df185a8624df5ad6e1627f40d61
Bruce Collie <brucecollie82@gmail.com>
2019-07-31 12:00:41 +0100

    Starting to implement affine fragment

--- src/synth/src/affine_fragment.cpp
+++ src/synth/src/affine_fragment.cpp
@@ -1,1 +1,30 @@
 #include "affine_fragment.h"
+
+namespace synth {
+
+affine_fragment::affine_fragment(
+    std::vector<props::value> args, frag_ptr before)
+    : fragment(args)
+    , before_(before)
+{
+}
+
+affine_fragment::affine_fragment(std::vector<props::value> args)
+    : affine_fragment(args, nullptr)
+{
+}
+
+std::string affine_fragment::to_str(size_t indent)
+{
+  return "AFFINE"; // TODO
+}
+
+size_t affine_fragment::count_holes() const { return count_or_empty(before_); }
+
+void swap(affine_fragment& a, affine_fragment& b)
+{
+  using std::swap;
+  swap(a.before_, b.before_);
+}
+
+} // namespace synth
