e5d46fa8dcd6dd92ac6eda29d5aaa9ff0485ff12
Bruce Collie <brucecollie82@gmail.com>
2019-08-07 15:20:47 +0100

    Better lerp, more lens

---
+++ src/synth/test/inputs/mf_length_sq.c
@@ -0,0 +1,8 @@
+void length_sq(float* x, int n)
+{
+  float sum = 0.0f;
+  for (int i = 0; i < n; ++i) {
+    sum += x[i] * x[i];
+  }
+  return sum;
+}
--- src/synth/test/inputs/mf_lerp.c
+++ src/synth/test/inputs/mf_lerp.c
@@ -1,6 +1,6 @@
 void lerp(float* o, float* x, float* y, float alpha, int n)
 {
   for (int i = 0; i < n; ++i) {
-    o[i] = alpha * x[i] + (1 - alpha) * y[i];
+    o[i] = x[i] + alpha * (y[i] - x[i]);
   }
 }
