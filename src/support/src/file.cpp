8779aaad2f611eea3910afc22c2370e4f10a256e
Bruce Collie <brucecollie82@gmail.com>
2019-08-08 08:59:05 +0100

    Throw an exception when we can't open a file

--- src/support/src/file.cpp
+++ src/support/src/file.cpp
@@ -2,6 +2,7 @@
 
 #include <llvm/Support/FileSystem.h>
 
+#include <sstream>
 #include <system_error>
 
 using namespace llvm;
@@ -12,6 +13,13 @@ std::unique_ptr<raw_fd_ostream> get_fd_ostream(std::string const& path)
 {
   auto err = std::error_code {};
   auto ptr = std::make_unique<raw_fd_ostream>(path, err, sys::fs::FA_Write);
+
+  if (err) {
+    auto ss = std::stringstream {};
+    ss << "Error opening file: " << err;
+    throw std::runtime_error(ss.str());
+  }
+
   return ptr;
 }
 
