cae889108286741b89ac21717e30fa0e7475245b
Bruce Collie <brucecollie82@gmail.com>
2019-08-08 10:52:10 +0100

    Some ARMs

---
+++ src/synth/test/inputs/arm_vadd.c
@@ -0,0 +1,6 @@
+void vadd(int n, float* a, float* b)
+{
+  for (int i = 0; i < n; ++i) {
+    a[i] += b[i];
+  }
+}
---
+++ src/synth/test/inputs/arm_vcopy.c
@@ -0,0 +1,6 @@
+void vcopy(int n, float* a, float* b)
+{
+  for (int i = 0; i < n; ++i) {
+    a[i] = b[i];
+  }
+}
