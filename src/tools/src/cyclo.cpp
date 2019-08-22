5a6635a68f08fe3bbdfc26a38b41e9f8d4ec51c4
Bruce Collie <brucecollie82@gmail.com>
2019-08-22 09:30:17 +0100

    Cyclo broken on mac

--- src/tools/src/cyclo.cpp
+++ src/tools/src/cyclo.cpp
@@ -1,1 +1,15 @@
-int main() {}
+#include <passes/passes.h>
+
+#include <llvm/Support/CommandLine.h>
+
+using namespace llvm;
+
+static cl::opt<std::string> InputFilename(cl::Positional,
+    cl::desc("<input bitcode file>"), cl::init("-"),
+    cl::value_desc("filename"));
+
+int main(int argc, char** argv)
+{
+  cl::ParseCommandLineOptions(argc, argv);
+  auto pass = createCycloPass();
+}
