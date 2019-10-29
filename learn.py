b1fbbe4c81fe457aa3be594310c59d229b17698f
Bruce Collie <brucecollie82@gmail.com>
2019-10-29 14:50:13 +0000

    Attempt to learn

--- learn.py
+++ learn.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python
 
 import graphviz
+import random
 from sklearn import tree
 
-# A,B,V = 0, 1, 2
-# ptr = 0, 1
-# out = 1 if V A0 B1
+DATA = './dataset.txt'
 
-A, B, V = 0, 1, 2
+def load_data():
+    ret = []
+    with open(DATA, 'r') as f:
+        for line in f:
+            ins, outs = line.split(' ')
+            ins = [int(x) for x in ins.split(',')]
+            outs = [int(x) for x in outs.split(',')]
+            ret.append((ins, outs))
+    return ret
+
+def err(real, pred):
+    s = 0
+    for r, p in zip(real, pred):
+        s += (r == p)
+    return 1 - (float(s) / len(real))
+
+def split(data, test = 0.1):
+    copy = data[:]
+    random.shuffle(copy)
+    n_test = int(test * len(data))
+    return copy[:n_test], copy[n_test:] 
+
+def xs(data):
+    return [p[0] for p in data]
+
+def ys(data):
+    return [p[1] for p in data]
 
 def main():
-    X = [
-        [V, 0, A, 0, B, 1],
-        [A, 1, A, 1, A, 1],
-        [B, 0, B, 0, B, 1],
-        [V, 1, A, 0, B, 1],
-        [V, 0, B, 1, A, 0]
-    ]
-    Y = [1, 0, 0, 0, 1]
-
+    data = load_data()
+    train, test = split(data)
     clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(X, Y)
-
-    dot_data = tree.export_graphviz(clf, out_file=None)
-    graph = graphviz.Source(dot_data)
-    graph.render('outf')
+    clf = clf.fit(xs(train), ys(train))
+    print(clf.predict_proba([xs(test)[1]]))
 
 if __name__ == "__main__":
     main()
