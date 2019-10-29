32f6c4e0a72a3f7ec2da5ae3e52b9ebcc01e3e65
Bruce Collie <brucecollie82@gmail.com>
2019-10-29 12:29:34 +0000

    Implement more formatters

--- src/predictor/src/main.cpp
+++ src/predictor/src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
   }
 
   auto sum = summary(all_props);
-  auto ps = all_props[0];
+  auto ps = all_props[25];
 
   fmt::print("{}\n\n{}\n", ps, sum.encode(ps));
 }
