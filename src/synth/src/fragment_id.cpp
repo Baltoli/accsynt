d0fdc16ff202b481e2342a64cfbc95baed7bdf82
Bruce Collie <brucecollie82@gmail.com>
2019-11-01 15:38:50 +0000

    Add search functionality

--- src/synth/src/fragment_id.cpp
+++ src/synth/src/fragment_id.cpp
@@ -1,4 +1,10 @@
 #include "fragment_id.h"
 
+namespace {
+
+std::set<char*> reg_ptrs_ = {};
+
+}
+
 namespace synth {
 }
--- src/synth/src/fragment_id.h
+++ src/synth/src/fragment_id.h
@@ -1,4 +1,26 @@
 #pragma once
 
-namespace synth {
+#include <algorithm>
+#include <set>
+
+namespace {
+
+extern std::set<char*> reg_ptrs_;
+
 }
+
+namespace synth {
+
+template <typename T>
+int get_id()
+{
+  return std::distance(reg_ptrs_.begin(), reg_ptrs_.find(&T::ID));
+}
+
+template <typename T>
+int get_id(T&& t)
+{
+  return get_id<T>();
+}
+
+} // namespace synth
