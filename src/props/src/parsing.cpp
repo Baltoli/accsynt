ae935808193cbffd5df63028ad8db1deb08c3c83
Bruce Collie <brucecollie82@gmail.com>
2019-11-11 12:13:44 +0000

    Make char a parse error

--- src/props/src/parsing.cpp
+++ src/props/src/parsing.cpp
@@ -11,7 +11,12 @@ using namespace pegtl;
 std::optional<data_type> data_type_from_string(std::string const& str)
 {
   if (str == "char") {
-    return data_type::character;
+    // Hack to disable character type on the baseline implementation - due to
+    // ICSE not being accepted, we can use PACT as the published baseline
+    // performance to compare against. It does not have chars, and the simplest
+    // way to emulate this without a huge restructure / git-fu is to make them a
+    // parse error.
+    return std::nullopt;
   } else if (str == "int") {
     return data_type::integer;
   } else if (str == "float") {
