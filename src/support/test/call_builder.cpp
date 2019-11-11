736ff4e66808af69aa64d3cc26b698d2e7cfe22c
Bruce Collie <brucecollie82@gmail.com>
2019-11-11 12:35:59 +0000

    Fix support unit

--- src/support/test/call_builder.cpp
+++ src/support/test/call_builder.cpp
@@ -97,7 +97,7 @@ TEST_CASE("Can construct call builders from signatures")
   auto s1 = "void f()"_sig;
   auto c1 = call_builder(s1);
 
-  auto s2 = "char c(int a, char b, float c)"_sig;
+  auto s2 = "int c(int a, int b, float c)"_sig;
   auto c2 = call_builder(s2);
 }
 
@@ -115,10 +115,6 @@ TEST_CASE("Can extract arguments from a call_builder")
     auto c3 = call_builder("void f(int x, int y, int z)"_sig);
     c3.add(0, 0);
     REQUIRE_THROWS_AS(c3.get<int>(2), call_builder_error);
-
-    auto c4 = call_builder("void f(char v, int g)"_sig);
-    c4.add('a', 78);
-    REQUIRE_THROWS_AS(c4.get<char>(2), call_builder_error);
   }
 
   SECTION("Can get ints back correctly")
@@ -138,29 +134,6 @@ TEST_CASE("Can extract arguments from a call_builder")
       c2.add(v2[0], 0.4f, v2[1]);
       REQUIRE(c2.get<int>(0) == v2[0]);
       REQUIRE(c2.get<int>(2) == v2[1]);
-    }
-  }
-
-  SECTION("Can get chars back correctly")
-  {
-    SECTION("A simple case")
-    {
-      auto c1 = call_builder("void f(char c)"_sig);
-      char v = GENERATE(ALLCHARS());
-      c1.add(v);
-      REQUIRE(c1.get<char>(0) == v);
-    }
-
-    SECTION("More args")
-    {
-      auto c2 = call_builder("int g(char c1, int x, float y, char c2)"_sig);
-
-      auto vs
-          = GENERATE_COPY(take(100, chunk(2, map<char>(to_c, ALLVS(char)))));
-
-      c2.add(vs[0], 0, 0.0f, vs[1]);
-      REQUIRE(c2.get<char>(0) == vs[0]);
-      REQUIRE(c2.get<char>(3) == vs[1]);
     }
   }
 
@@ -201,21 +174,6 @@ TEST_CASE("Can extract arguments from a call_builder")
       REQUIRE(ext_vec.data() != vec.data());
     }
 
-    SECTION("For chars")
-    {
-      auto c = call_builder("void z(char *cs)"_sig);
-
-      auto len = GENERATE(take(1, random(1, 1000)));
-      auto vec
-          = GENERATE_COPY(take(100, chunk(len, map<char>(to_c, ALLVS(char)))));
-
-      c.add(vec);
-
-      auto ext_vec = c.get<std::vector<char>>(0);
-      REQUIRE(ext_vec == vec);
-      REQUIRE(ext_vec.data() != vec.data());
-    }
-
     SECTION("For floats")
     {
       auto c1 = call_builder("void h(float *x)"_sig);
@@ -233,37 +191,32 @@ TEST_CASE("Can extract arguments from a call_builder")
 
   SECTION("Can get whole signatures back correctly")
   {
-    auto c1
-        = call_builder("int fun(int a, int *b, float *c, float d, char e)"_sig);
+    auto c1 = call_builder("int fun(int a, int *b, float *c, float d)"_sig);
     auto a = 784123;
     auto b = std::vector{ 1, 2, 3 };
     auto c = std::vector{ 0.4f, 0.2f, 453.2f };
     auto d = 5768.11f;
-    auto e = '{';
-    c1.add(a, b, c, d, e);
+    c1.add(a, b, c, d);
 
     REQUIRE(c1.get<int>(0) == a);
     REQUIRE(c1.get<std::vector<int>>(1) == b);
     REQUIRE(c1.get<std::vector<float>>(2) == c);
     REQUIRE(c1.get<float>(3) == d);
-    REQUIRE(c1.get<char>(4) == e);
   }
 
   SECTION("Can get parameters back by name")
   {
     auto c1 = call_builder(
-        "int fun(int *aio_efd, int woo, float *c__a, float doo, char eep)"_sig);
+        "int fun(int *aio_efd, int woo, float *c__a, float doo)"_sig);
     auto aio_efd = std::vector{ 1, 2, 3 };
     auto woo = 784123;
     auto c__a = std::vector{ 0.4f, 0.2f, 453.2f };
     auto doo = 5768.11f;
-    auto eep = '{';
-    c1.add(aio_efd, woo, c__a, doo, eep);
+    c1.add(aio_efd, woo, c__a, doo);
 
     REQUIRE(c1.get<std::vector<int>>("aio_efd") == aio_efd);
     REQUIRE(c1.get<int>("woo") == woo);
     REQUIRE(c1.get<std::vector<float>>("c__a") == c__a);
     REQUIRE(c1.get<float>("doo") == doo);
-    REQUIRE(c1.get<char>("eep") == eep);
   }
 }
