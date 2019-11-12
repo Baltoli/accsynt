dbd42566a1dcffa6d796c9c81462e62cdbd4a256
Bruce Collie <brucecollie82@gmail.com>
2019-11-12 14:24:14 +0000

    Silence two minor warnings

--- src/synth/test/inputs/relu-over.c
+++ src/synth/test/inputs/relu-over.c
@@ -1,8 +1,7 @@
-void relu(int n, float *o)
+void relu(int n, float* o)
 {
-  for(int i = 0; i < n; ++i) {
-    float val = o[i];
-    if(o[i] < 0) {
+  for (int i = 0; i < n; ++i) {
+    if (o[i] < 0) {
       o[i] = 0.0f;
     }
   }
