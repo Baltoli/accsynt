14956448b80b01a938e390b06658c411fdccb962
Bruce Collie <brucecollie82@gmail.com>
2019-11-12 14:18:11 +0000

    Remove ifind lib

--- src/ifind/test/inputs/branch.c
+++
@@ -1,8 +0,0 @@
-float test(float a, float b)
-{
-  if(a > b) {
-    return a + b;
-  } else {
-    return b;
-  }
-}
--- src/ifind/test/inputs/vsum.c
+++
@@ -1,8 +0,0 @@
-float vsum(float* input, int n)
-{
-  float sum = 0.0f;
-  for (int i = 0; i < n; ++i) {
-    sum += input[i];
-  }
-  return sum;
-}
