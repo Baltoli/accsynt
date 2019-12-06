96c820dcf701192e2b024094ee32ca3717d2503c
Bruce Collie <brucecollie82@gmail.com>
2019-12-06 09:58:23 +0000

    Disable clang tidy by default

--- src/cmake/modules/ClangTidy.cmake
+++ src/cmake/modules/ClangTidy.cmake
@@ -1,28 +1,32 @@
-include(ListJoin)
+option(USE_CLANG_TIDY "Enable clang-tidy checking for this project" OFF)
 
-find_program(
-  CLANG_TIDY_EXE
-  "clang-tidy"
-  HINTS "${LLVM_INSTALL_PREFIX}/bin"
-)
+if(USE_CLANG_TIDY)
+  include(ListJoin)
 
-set(CLANG_TIDY_CHECKS_LIST
-  "-*"
-  "bugprone-*"
-  "-bugprone-exception-escape"
-  "-bugprone-macro-parentheses"
-  "-bugprone-narrowing-conversions"
-  "-bugprone-sizeof-expression"
-)
-JOIN("${CLANG_TIDY_CHECKS_LIST}" "," CLANG_TIDY_CHECKS)
-
-if(CLANG_TIDY_EXE)
-  set(CLANG_TIDY 
-    "${CLANG_TIDY_EXE}"
-    "-checks=${CLANG_TIDY_CHECKS}"
+  find_program(
+    CLANG_TIDY_EXE
+    "clang-tidy"
+    HINTS "${LLVM_INSTALL_PREFIX}/bin"
   )
 
-  set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY}")
-else()
-  message(WARNING "No clang-tidy found")
+  set(CLANG_TIDY_CHECKS_LIST
+    "-*"
+    "bugprone-*"
+    "-bugprone-exception-escape"
+    "-bugprone-macro-parentheses"
+    "-bugprone-narrowing-conversions"
+    "-bugprone-sizeof-expression"
+  )
+  JOIN("${CLANG_TIDY_CHECKS_LIST}" "," CLANG_TIDY_CHECKS)
+
+  if(CLANG_TIDY_EXE)
+    set(CLANG_TIDY 
+      "${CLANG_TIDY_EXE}"
+      "-checks=${CLANG_TIDY_CHECKS}"
+    )
+
+    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY}")
+  else()
+    message(WARNING "No clang-tidy found")
+  endif()
 endif()
