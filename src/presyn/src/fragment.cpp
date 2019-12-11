fbaa6c3d5784c2e4dbf20cc3f993b8804f9bdd9b
Bruce Collie <brucecollie82@gmail.com>
2019-12-11 11:57:56 +0000

    Format linear frags

--- src/presyn/src/fragment.cpp
+++ src/presyn/src/fragment.cpp
@@ -1,5 +1,7 @@
 #include "fragment.h"
 
+#include <fmt/format.h>
+
 namespace presyn {
 
 // Empty
@@ -31,6 +33,10 @@ std::unique_ptr<fragment> linear::compose(std::unique_ptr<fragment>&& other)
 
 bool linear::accepts() const { return false; }
 
-std::string linear::to_string() const { return "linear()"; }
+std::string linear::to_string() const
+{
+  using namespace fmt::literals;
+  return "linear({})"_format(instructions_);
+}
 
 } // namespace presyn
