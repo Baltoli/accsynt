eb9e96a999bc4983e85ce7735707818354576565
Bruce Collie <brucecollie82@gmail.com>
2020-01-07 11:47:20 +0000

    Add first grammar rule

--- src/presyn/src/fragment_parse.h
+++ src/presyn/src/fragment_parse.h
@@ -1,7 +1,23 @@
 #pragma once
 
-#define TAO_PEGTL_NAMESPACE presyn_pegtl
+#define TAO_PEGTL_NAMESPACE pre_tl
 #include <tao/pegtl.hpp>
 
-namespace presyn {
-}
+namespace presyn::grammar {
+
+// Grammars make such horrible template invocations that it's best to format
+// them ourselves
+
+// clang-format off
+
+struct fragment_name : 
+  tao::pre_tl::sor<
+    TAO_PEGTL_STRING("linear"),
+    TAO_PEGTL_STRING("empty"), 
+    TAO_PEGTL_STRING("seq")
+  > 
+{};
+
+// clang-format on
+
+} // namespace presyn::grammar
--- src/presyn/test/parsing.cpp
+++ src/presyn/test/parsing.cpp
@@ -1,3 +1,8 @@
 #include "fragment_parse.h"
 
 #include <catch2/catch.hpp>
+
+using namespace tao::pre_tl;
+using namespace presyn::grammar;
+
+TEST_CASE("Can recognise fragment names") {}
