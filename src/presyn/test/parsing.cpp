2db2c33bbef0d505a60995dd14724b9b40e14124
Bruce Collie <brucecollie82@gmail.com>
2020-01-07 11:57:37 +0000

    Implement and test fragment grammar

--- src/presyn/test/parsing.cpp
+++ src/presyn/test/parsing.cpp
@@ -5,4 +5,30 @@
 using namespace tao::pre_tl;
 using namespace presyn::grammar;
 
-TEST_CASE("Can recognise fragment names") {}
+#define MATCH_EXACT(grammar, str)                                              \
+  do {                                                                         \
+    REQUIRE(parse<seq<grammar, eof>>(string_input(str, __func__)));            \
+  } while (false)
+
+#define NOT_MATCH_EXACT(grammar, str)                                          \
+  do {                                                                         \
+    REQUIRE(!parse<seq<grammar, eof>>(string_input(str, __func__)));           \
+  } while (false)
+
+TEST_CASE("Can recognise fragment names")
+{
+  SECTION("Can correctly recognise valid names")
+  {
+    MATCH_EXACT(fragment_name, "linear");
+    MATCH_EXACT(fragment_name, "empty");
+    MATCH_EXACT(fragment_name, "seq");
+  }
+
+  SECTION("Does not match incorrect names")
+  {
+    NOT_MATCH_EXACT(fragment_name, "fweo");
+    NOT_MATCH_EXACT(fragment_name, "sequence");
+    NOT_MATCH_EXACT(fragment_name, "empty_fragment");
+    NOT_MATCH_EXACT(fragment_name, "");
+  }
+}
