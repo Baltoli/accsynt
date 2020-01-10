544721ce6104c7178e3be409e81f7eb9d52f68ef
Bruce Collie <brucecollie82@gmail.com>
2020-01-10 13:54:57 +0000

    Reword test

--- src/presyn/test/regression.cpp
+++ src/presyn/test/regression.cpp
@@ -10,7 +10,7 @@ using namespace presyn;
 using namespace presyn::literals;
 using namespace std::string_literals;
 
-TEST_CASE("Parse building using composition breaks on empty")
+TEST_CASE("Parse building using composition does not break on empty")
 {
   auto cases = std::array{"seq(empty, linear<2>)"s,
                           "if_else(empty, seq(empty, loop(empty)))"s};
