370f8403028b20322b5c27cb2c3cddccef9ef538
Bruce Collie <brucecollie82@gmail.com>
2020-03-13 14:41:34 +0000

    Add call to instrument function

--- src/coverage/include/coverage/coverage.h
+++ src/coverage/include/coverage/coverage.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <support/call_builder.h>
 #include <support/call_wrapper.h>
 
 namespace coverage {
 
 class wrapper : public support::call_wrapper {
+public:
   using support::call_wrapper::call_wrapper;
+
+  uint64_t call(support::call_builder& builder);
 
 private:
   bool instrumented_ = false;
--- src/coverage/src/wrapper.cpp
+++ src/coverage/src/wrapper.cpp
@@ -1,4 +1,16 @@
 #include <coverage/coverage.h>
 
 namespace coverage {
+
+uint64_t wrapper::call(support::call_builder& builder)
+{
+  if (!instrumented_) {
+    instrument();
+  }
+
+  return support::call_wrapper::call(builder);
 }
+
+void wrapper::instrument() { instrumented_ = true; }
+
+} // namespace coverage
