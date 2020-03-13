09c1e12bc04b959c8053225e126fa321ef6ac3f4
Bruce Collie <brucecollie82@gmail.com>
2020-03-13 14:29:33 +0000

    Start on wrapper impl

--- src/coverage/include/coverage/coverage.h
+++ src/coverage/include/coverage/coverage.h
@@ -6,6 +6,10 @@ namespace coverage {
 
 class wrapper : public support::call_wrapper {
   using support::call_wrapper::call_wrapper;
+
+private:
+  bool instrumented_ = false;
+  void instrument();
 };
 
 } // namespace coverage
---
+++ src/coverage/src/wrapper.cpp
@@ -0,0 +1,4 @@
+#include <coverage/coverage.h>
+
+namespace coverage {
+}
