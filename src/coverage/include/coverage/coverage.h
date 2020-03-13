d6ae95a3b6a27954dbe668e6e9c03021d15748d0
Bruce Collie <brucecollie82@gmail.com>
2020-03-13 14:23:10 +0000

    Make basic calls to the new wrapper

--- src/coverage/include/coverage/coverage.h
+++ src/coverage/include/coverage/coverage.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <support/call_wrapper.h>
+
+namespace coverage {
+
+class wrapper : public support::call_wrapper {
+  using support::call_wrapper::call_wrapper;
+};
+
+} // namespace coverage
---
+++ src/coverage/test/wrapper.cpp
@@ -0,0 +1,64 @@
+#include <coverage/coverage.h>
+
+#include <props/props.h>
+
+#include <support/load_module.h>
+
+#include <catch2/catch.hpp>
+
+using namespace props::literals;
+
+auto count_negs = R"(
+define i32 @count_negs(i32, float*) {
+  br label %3
+
+3:                                                ; preds = %13, %2
+  %.01 = phi i32 [ 0, %2 ], [ %.1, %13 ]
+  %.0 = phi i32 [ 0, %2 ], [ %14, %13 ]
+  %4 = icmp slt i32 %.0, %0
+  br i1 %4, label %5, label %15
+
+5:                                                ; preds = %3
+  %6 = sext i32 %.0 to i64
+  %7 = getelementptr inbounds float, float* %1, i64 %6
+  %8 = load float, float* %7, align 4
+  %9 = fcmp olt float %8, 0.000000e+00
+  br i1 %9, label %10, label %12
+
+10:                                               ; preds = %5
+  %11 = add nsw i32 %.01, 1
+  br label %12
+
+12:                                               ; preds = %10, %5
+  %.1 = phi i32 [ %11, %10 ], [ %.01, %5 ]
+  br label %13
+
+13:                                               ; preds = %12
+  %14 = add nsw i32 %.0, 1
+  br label %3
+
+15:                                               ; preds = %3
+  ret i32 %.01
+}
+)";
+
+TEST_CASE("Can construct coverage wrappers")
+{
+  PARSE_TEST_MODULE(mod, count_negs);
+
+  auto wrap = coverage::wrapper(
+      "int count_negs(int n, float *xs)"_sig, *mod, "count_negs");
+}
+
+TEST_CASE("Can get builders and make calls to wrappers")
+{
+  PARSE_TEST_MODULE(mod, count_negs);
+
+  auto wrap = coverage::wrapper(
+      "int count_negs(int n, float *xs)"_sig, *mod, "count_negs");
+
+  auto b = wrap.get_builder();
+  b.add(4, std::vector<float>{1, -1, 2, -2});
+
+  REQUIRE(wrap.call(b) == 2);
+}
