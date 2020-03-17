503a90de09776350e7cf24668a3007960e4ff0a3
Bruce Collie <brucecollie82@gmail.com>
2020-03-17 11:13:07 +0000

    Declare trampoline function

--- src/coverage/src/wrapper.cpp
+++ src/coverage/src/wrapper.cpp
@@ -55,14 +55,29 @@ void wrapper::instrument()
   using namespace std::placeholders;
 
   auto& ctx = support::thread_context::get();
+  auto mod = implementation()->getParent();
 
   auto i8_t = llvm::IntegerType::get(ctx, 8);
   auto p_i8_t = llvm::PointerType::getUnqual(i8_t);
 
+  // Add pointer back to this instance of the wrapper class so that we can call
+  // the trampoline properly.
   instance_ptr_ = new llvm::GlobalVariable(
-      *implementation()->getParent(), p_i8_t, true,
-      llvm::GlobalValue::ExternalLinkage, nullptr, "instance");
+      *mod, p_i8_t, true, llvm::GlobalValue::ExternalLinkage, nullptr,
+      "instance");
   engine()->addGlobalMapping(instance_ptr_, (void*)this);
+
+  // Create another global mapping for the trampoline function. The visitor then
+  // receives this external function as its callback to insert.
+  auto i32_t = llvm::IntegerType::get(ctx, 32);
+  auto bool_t = llvm::IntegerType::get(ctx, 1);
+  auto void_t = llvm::Type::getVoidTy(ctx);
+  auto trampoline_t
+      = llvm::FunctionType::get(void_t, {i32_t, bool_t, p_i8_t}, false);
+
+  auto func = llvm::Function::Create(
+      trampoline_t, llvm::GlobalValue::ExternalLinkage, "trampoline", *mod);
+  engine()->addGlobalMapping(func, (void*)handle_branch_event_trampoline);
 
   instrument_visitor(branch_ids_).visit(implementation());
 
