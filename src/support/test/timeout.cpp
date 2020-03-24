eccc7de1e686b104f8992d5b0c595fc07b63c134
Bruce Collie <brucecollie82@gmail.com>
2020-03-24 19:42:01 +0000

    Tests for timeout

---
+++ src/support/test/timeout.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch.hpp>
+
+#include <support/timeout.h>
+
+using namespace support;
+
+TEST_CASE("Can run short operations that don't time out") {}
+
+TEST_CASE("Can run longer operations") {}
