d0c1348169fa0b0af3ce79463f387efba3ef1803
Bruce Collie <brucecollie82@gmail.com>
2020-06-11 17:01:39 +0100

    Minimal interface, call from REPL

--- src/presyn/src/random_filler.cpp
+++ src/presyn/src/random_filler.cpp
@@ -1,4 +1,12 @@
 #include "random_filler.h"
 
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Value.h>
+
+using namespace llvm;
+
 namespace presyn {
-}
+
+Value* random_filler::fill(CallInst* hole) { return nullptr; }
+
+} // namespace presyn
--- src/presyn/src/random_filler.h
+++ src/presyn/src/random_filler.h
@@ -1,4 +1,15 @@
 #pragma once
 
+#include "filler.h"
+
 namespace presyn {
-}
+
+class random_filler : public filler {
+public:
+  random_filler() = default;
+
+protected:
+  llvm::Value* fill(llvm::CallInst*) override;
+};
+
+} // namespace presyn
