6dbb0b190f2dfe3880d38d4de12fd51287f2d541
Bruce Collie <brucecollie82@gmail.com>
2020-06-11 11:19:09 +0100

    Implement pure virtual filler interface

--- src/presyn/src/filler.h
+++ src/presyn/src/filler.h
@@ -1,4 +1,24 @@
 #pragma once
 
+#include "candidate.h"
+
+#include <llvm/IR/Value.h>
+
 namespace presyn {
-}
+
+/**
+ * Interface that represents something capable of choosing values for a
+ * particular hole. The only operation publicly visible is construction;
+ * everything else is handled by passing it to a newly constructed candidate.
+ */
+class filler {
+  friend class candidate;
+
+public:
+  virtual ~filler() = default;
+
+protected:
+  virtual llvm::Value* fill(llvm::Value*) = 0;
+};
+
+} // namespace presyn
