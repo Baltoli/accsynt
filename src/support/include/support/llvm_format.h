1ca223c0db374e11ff3fe0f9c6a2c1391c7afb6b
Bruce Collie <brucecollie82@gmail.com>
2020-06-12 08:50:41 +0100

    Unify LLVM formatting to one implementation

--- src/support/include/support/llvm_format.h
+++ src/support/include/support/llvm_format.h
@@ -11,28 +11,25 @@
 #include <type_traits>
 #include <variant>
 
-template <>
-struct fmt::formatter<llvm::Module> {
-  template <typename ParseContext>
-  constexpr auto parse(ParseContext& ctx)
-  {
-    return ctx.begin();
-  }
+namespace support::detail {
 
-  template <typename FormatContext>
-  auto format(llvm::Module const& v, FormatContext& ctx)
-  {
-    auto str = std::string {};
-    auto os = llvm::raw_string_ostream(str);
-
-    os << v;
-    return format_to(ctx.out(), "{}", ::support::left_trim(str));
-  }
+template <typename T>
+struct llvm_formattable {
+  // Ideally this would be a concept restricting to things that permit streaming
+  // to a raw_string_ostream, but for now this check catches both of the cases
+  // we care about while using only one formatter implementation.
+  static constexpr bool value
+      = std::is_same_v<llvm::Module, T> || std::is_base_of_v<llvm::Value, T>;
 };
+
+template <typename T>
+constexpr inline bool llvm_formattable_v = llvm_formattable<T>::value;
+
+} // namespace support::detail
 
 template <typename Val>
 struct fmt::formatter<
-    Val, std::enable_if_t<std::is_base_of_v<llvm::Value, Val>, char>> {
+    Val, std::enable_if_t<support::detail::llvm_formattable_v<Val>, char>> {
   template <typename ParseContext>
   constexpr auto parse(ParseContext& ctx)
   {
