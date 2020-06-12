6a613adb3deb360621479332609368288ef09b58
Bruce Collie <brucecollie82@gmail.com>
2020-06-12 11:04:51 +0100

    Basic null constants

--- src/presyn/src/constants.cpp
+++ src/presyn/src/constants.cpp
@@ -1,4 +1,14 @@
 #include "constants.h"
 
+#include <llvm/IR/Constant.h>
+
+using namespace llvm;
+
 namespace presyn {
+
+std::vector<llvm::Value*> small_constants(llvm::Type* ty)
+{
+  return {Constant::getNullValue(ty)};
 }
+
+} // namespace presyn
--- src/presyn/src/constants.h
+++ src/presyn/src/constants.h
@@ -1,4 +1,12 @@
 #pragma once
 
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Value.h>
+
+#include <vector>
+
 namespace presyn {
+
+std::vector<llvm::Value*> small_constants(llvm::Type*);
+
 }
