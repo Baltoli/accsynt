dc1f6a38a6eecc419d10bab8850e4f2d8617fec4
Bruce Collie <brucecollie82@gmail.com>
2020-06-16 09:39:49 +0100

    Implement first of type rule

--- src/presyn/src/rules.cpp
+++ src/presyn/src/rules.cpp
@@ -1,5 +1,7 @@
 #include "rules.h"
 
+#include <support/assert.h>
+
 namespace presyn {
 
 void first_of_type::match(
@@ -7,6 +9,18 @@ void first_of_type::match(
     std::vector<llvm::Value*> const& choices,
     std::vector<llvm::Value*>& ret) const
 {
+  assumes(!choices.empty(), "Must have some choices");
+
+  if (fill.has_unknown_type(hole)) {
+    ret.push_back(choices[0]);
+  } else {
+    for (auto c : choices) {
+      if (c->getType() == hole->getType()) {
+        ret.push_back(c);
+        break;
+      }
+    }
+  }
 }
 
 } // namespace presyn
