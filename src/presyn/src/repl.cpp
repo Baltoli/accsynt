f646ead3c2a73fbe4f1679d4b1ecedd57b9018a8
Bruce Collie <brucecollie82@gmail.com>
2020-10-16 14:24:15 +0100

    Abstract getting input

--- src/presyn/src/repl.cpp
+++ src/presyn/src/repl.cpp
@@ -5,44 +5,22 @@
 
 #include <props/props.h>
 
+#include <support/input.h>
 #include <support/llvm_format.h>
 
 #include <fmt/format.h>
 
-#include <readline/history.h>
-#include <readline/readline.h>
-
-#include <iostream>
-
 using namespace presyn;
-
-std::string get_line(std::string const& prompt)
-{
-  char* buf = readline(prompt.c_str());
-
-  if (!buf) {
-    return "";
-  }
-
-  auto str = std::string(buf);
-  free(buf);
-
-  if (str.size() > 0) {
-    add_history(str.c_str());
-  }
-
-  return str;
-}
 
 int main()
 {
   std::unique_ptr<fragment> current_frag = std::make_unique<hole>();
 
-  auto sig_line = get_line(" sig> ");
+  auto sig_line = support::get_line(" sig> ");
   auto sig = props::signature::parse(sig_line);
 
   while (true) {
-    auto line = get_line("frag> ");
+    auto line = support::get_line("frag> ");
 
     // Break out of the REPL if we get ^D or an empty input
     if (line.empty()) {
--- src/support/include/support/input.h
+++ src/support/include/support/input.h
@@ -1,4 +1,10 @@
 #pragma once
 
+#include <string>
+#include <string_view>
+
 namespace support {
+
+std::string get_line(std::string const& prompt);
+
 }
