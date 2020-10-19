86922cf878c2128ca08066075422bee60a10dad7
Bruce Collie <brucecollie82@gmail.com>
2020-10-19 10:08:32 +0100

    Figure out rule matching interface

--- src/presyn/src/rules.h
+++ src/presyn/src/rules.h
@@ -1,9 +1,25 @@
 #pragma once
 
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Value.h>
+
 #include <tuple>
+#include <vector>
 
 namespace presyn {
 
-inline auto all_rules() { return std::tuple {}; }
+namespace rules {
+
+struct do_nothing {
+  void match(
+      llvm::CallInst* hole, std::vector<llvm::Value*> const& choices,
+      std::vector<llvm::Value*>& generated) const
+  {
+  }
+};
+
+} // namespace rules
+
+inline auto all_rules() { return std::tuple {rules::do_nothing {}}; }
 
 } // namespace presyn
