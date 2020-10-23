f4a3931cd95a8f94f01c46ef5609c9ee2ca3daa4
Bruce Collie <brucecollie82@gmail.com>
2020-10-23 10:18:59 +0100

    Add opaque registration function

--- src/support/include/support/llvm_types.h
+++ src/support/include/support/llvm_types.h
@@ -1,4 +1,20 @@
 #pragma once
 
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Value.h>
+
+#include <set>
+
 namespace support {
-}
+
+struct type_conversions {
+public:
+  type_conversions() = default;
+
+  void register_opaque(llvm::Type*);
+
+private:
+  std::set<llvm::Type*> opaque_types_;
+};
+
+} // namespace support
--- src/support/src/llvm_types.cpp
+++ src/support/src/llvm_types.cpp
@@ -1,4 +1,10 @@
 #include <support/llvm_types.h>
 
 namespace support {
+
+void type_conversions::register_opaque(llvm::Type* op)
+{
+  opaque_types_.insert(op);
 }
+
+} // namespace support
