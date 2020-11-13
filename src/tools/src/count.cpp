53d5e59367e8287a86ebc560446ab2c19a92595a
Bruce Collie <brucecollie82@gmail.com>
2020-11-13 14:02:18 +0000

    Command line shell

--- src/tools/src/count.cpp
+++ src/tools/src/count.cpp
@@ -1,1 +1,27 @@
-int main() { }
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+
+static cl::opt<std::string> InputFilename(
+    cl::Positional, cl::desc("<input bitcode file>"), cl::init("-"),
+    cl::value_desc("filename"));
+
+int main(int argc, char** argv)
+{
+  cl::ParseCommandLineOptions(argc, argv);
+
+  LLVMContext Context;
+  SMDiagnostic Err;
+
+  auto&& mod = parseIRFile(InputFilename, Err, Context, true, "");
+  if (!mod) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
+}
