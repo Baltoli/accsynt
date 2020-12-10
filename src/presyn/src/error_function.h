b65d070974dfd497ae211469f0af667b46bc65a3
Bruce Collie <brucecollie82@gmail.com>
2020-12-10 11:27:54 +0000

    Implement error function by calling two wrappers

--- src/presyn/src/error_function.h
+++ src/presyn/src/error_function.h
@@ -3,6 +3,8 @@
 #include <support/call_builder.h>
 #include <support/call_wrapper.h>
 
+#include <functional>
+
 namespace presyn {
 
 int scalar_distance_error(
@@ -14,7 +16,13 @@ int compute_error(
     ErrF&& err, support::call_builder args, support::call_wrapper& f1,
     support::call_wrapper& f2)
 {
-  return 0;
+  auto a1 = args;
+  auto a2 = args;
+
+  auto r1 = f1.call(a1);
+  auto r2 = f2.call(a2);
+
+  return err({r1, a1}, {r2, a2});
 }
 
 } // namespace presyn
