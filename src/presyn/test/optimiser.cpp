b134c2c8581e0f2493f515a91ff7d264deaf088b
Bruce Collie <brucecollie82@gmail.com>
2020-12-14 16:03:44 +0000

    Construct target and reference

--- src/presyn/test/optimiser.cpp
+++ src/presyn/test/optimiser.cpp
@@ -4,6 +4,7 @@
 
 #include <holes/holes.h>
 
+#include <support/call_wrapper.h>
 #include <support/llvm_format.h>
 #include <support/thread_context.h>
 
@@ -17,17 +18,16 @@
 using namespace support;
 using namespace llvm;
 
-TEST_CASE("Scratch tests for optimiser")
+Function* create_target(Module& mod, holes::provider& hp)
 {
-  auto& ctx = thread_context::get();
-  auto mod = Module("test-opt", ctx);
+  auto& ctx = mod.getContext();
+
   auto int64_ty = IntegerType::get(ctx, 64);
   auto f_ty = FunctionType::get(int64_ty, {int64_ty}, false);
-  auto func = Function::Create(f_ty, GlobalValue::InternalLinkage, "ref", mod);
+  auto func
+      = Function::Create(f_ty, GlobalValue::InternalLinkage, "target", mod);
 
   auto bb = BasicBlock::Create(ctx, "linear", func);
-
-  auto hp = holes::provider(ctx, mod);
 
   for (auto i = 0; i < 3; ++i) {
     auto hole = hp.create_hole();
@@ -37,6 +37,40 @@ TEST_CASE("Scratch tests for optimiser")
   auto ret_hole = hp.create_hole(int64_ty);
   IRBuilder<>(bb).Insert(ret_hole);
   ReturnInst::Create(ctx, ret_hole, bb);
+
+  return func;
+}
+
+Function* create_ref(Module& mod)
+{
+  auto& ctx = mod.getContext();
+
+  auto int64_ty = IntegerType::get(ctx, 64);
+  auto f_ty = FunctionType::get(int64_ty, {int64_ty}, false);
+  auto func = Function::Create(f_ty, GlobalValue::InternalLinkage, "ref", mod);
+
+  auto bb = BasicBlock::Create(ctx, "linear", func);
+
+  auto B = IRBuilder<>(bb);
+  auto add = B.CreateAdd(func->getArg(0), B.getInt64(7));
+  auto mul = B.CreateMul(add, B.getInt64(3));
+  B.CreateRet(mul);
+
+  return func;
+}
+
+TEST_CASE("Scratch tests for optimiser")
+{
+  auto& ctx = thread_context::get();
+  auto mod = Module("test-opt", ctx);
+  auto hp = holes::provider(ctx, mod);
+
+  auto target = create_target(mod, hp);
+  auto optim = presyn::optimiser(target, std::move(hp));
+
+  auto ref = create_ref(mod);
+  auto wrap = call_wrapper(*ref);
+  optim.run(wrap);
 
   fmt::print("{}\n", mod);
 }
