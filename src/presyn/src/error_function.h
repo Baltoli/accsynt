83464c06c6f0b26896d2aa03d860870762a29e0a
Bruce Collie <brucecollie82@gmail.com>
2021-01-08 09:53:32 +0000

    Add some checks and genericity to compute_error

--- src/presyn/src/error_function.h
+++ src/presyn/src/error_function.h
@@ -4,6 +4,7 @@
 #include <support/call_wrapper.h>
 
 #include <functional>
+#include <type_traits>
 
 namespace presyn {
 
@@ -16,13 +17,20 @@ int compute_error(
     ErrF&& err, support::call_builder args, support::call_wrapper& f1,
     support::call_wrapper& f2)
 {
+  static_assert(
+      std::is_invocable_r_v<
+          int, ErrF, support::output_example, support::output_example>,
+      "Must be able to invoke error function with two output examples");
+
   auto a1 = args;
   auto a2 = args;
 
   auto r1 = f1.call(a1);
   auto r2 = f2.call(a2);
 
-  return err({r1, a1}, {r2, a2});
+  return std::invoke(
+      std::forward<ErrF>(err), support::output_example {r1, a1},
+      support::output_example {r2, a2});
 }
 
 } // namespace presyn
