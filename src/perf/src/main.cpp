85ff6696cf4c5d1669dfffd0479b91e84d5f0170
Bruce Collie <brucecollie82@gmail.com>
2021-02-15 10:51:41 +0000

    Adapt synth loading code

--- src/perf/src/main.cpp
+++ src/perf/src/main.cpp
@@ -13,10 +13,25 @@ using namespace support;
 using namespace llvm;
 
 int main(int argc, char** argv)
-{
+try {
   LLVMInitializeNativeTarget();
   LLVMInitializeNativeAsmPrinter();
   LLVMInitializeNativeAsmParser();
 
   cl::ParseCommandLineOptions(argc, argv);
+
+  auto property_set = props::property_set::load(PropertiesPath);
+  auto lib = dynamic_library(LibraryPath);
+
+  /* auto mod = Module("test_mod", thread_context::get()); */
+
+  /* auto ref = call_wrapper(property_set.type_signature, mod, fn_name, lib); */
+} catch (props::parse_error& perr) {
+  errs() << perr.what() << '\n';
+  errs() << "  when parsing property set " << PropertiesPath << '\n';
+  return 2;
+} catch (dyld_error& derr) {
+  errs() << derr.what() << '\n';
+  errs() << "  when loading dynamic library " << LibraryPath << '\n';
+  return 3;
 }
