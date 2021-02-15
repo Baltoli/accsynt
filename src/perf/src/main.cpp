e7fb6fd3227c91ce48010f56a40dcef9553a1a93
Bruce Collie <brucecollie82@gmail.com>
2021-02-15 10:24:30 +0000

    Boilerplate

--- src/perf/src/main.cpp
+++ src/perf/src/main.cpp
@@ -1,1 +1,22 @@
-int main() { }
+#include "options.h"
+
+#include <props/props.h>
+
+#include <support/call_wrapper.h>
+#include <support/dynamic_library.h>
+
+#include <fmt/format.h>
+
+#include <llvm/Support/CommandLine.h>
+
+using namespace support;
+using namespace llvm;
+
+int main(int argc, char** argv)
+{
+  LLVMInitializeNativeTarget();
+  LLVMInitializeNativeAsmPrinter();
+  LLVMInitializeNativeAsmParser();
+
+  cl::ParseCommandLineOptions(argc, argv);
+}
---
+++ src/perf/src/options.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include <llvm/Support/CommandLine.h>
