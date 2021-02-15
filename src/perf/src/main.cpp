a91b44ab829fbd3c9b721de8504c9d2229507f77
Bruce Collie <brucecollie82@gmail.com>
2021-02-15 11:02:28 +0000

    Basic implementation

--- src/perf/src/main.cpp
+++ src/perf/src/main.cpp
@@ -2,11 +2,16 @@
 
 #include <props/props.h>
 
+#include <support/argument_generator.h>
+#include <support/bit_cast.h>
 #include <support/call_wrapper.h>
 #include <support/dynamic_library.h>
+#include <support/llvm_format.h>
+#include <support/thread_context.h>
 
 #include <fmt/format.h>
 
+#include <llvm/IR/Module.h>
 #include <llvm/Support/CommandLine.h>
 
 using namespace support;
@@ -21,11 +26,21 @@ try {
   cl::ParseCommandLineOptions(argc, argv);
 
   auto property_set = props::property_set::load(PropertiesPath);
+  auto fn_name = property_set.type_signature.name;
+
   auto lib = dynamic_library(LibraryPath);
 
-  /* auto mod = Module("test_mod", thread_context::get()); */
+  auto mod = Module("perf_internal", thread_context::get());
+  auto ref = call_wrapper(property_set.type_signature, mod, fn_name, lib);
 
-  /* auto ref = call_wrapper(property_set.type_signature, mod, fn_name, lib); */
+  auto gen = uniform_generator();
+
+  auto b = ref.get_builder();
+  gen.gen_args(b);
+  auto res = ref.call(b);
+
+  fmt::print("{}\n", bit_cast<float>(res));
+
 } catch (props::parse_error& perr) {
   errs() << perr.what() << '\n';
   errs() << "  when parsing property set " << PropertiesPath << '\n';
