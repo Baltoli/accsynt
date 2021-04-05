02d3272686969d4998e0e785776bd643d4a0bb36
Bruce Collie <brucecollie82@gmail.com>
2021-04-05 17:28:28 +0100

    Some results

--- scripts/regression.py
+++ scripts/regression.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+import scipy.optimize as opt
+import sys
+
+def linear(x, lin):
+    return lin * x
+
+def square(x, coef):
+    return coef * (x ** 2)
+
+def log(x, coef):
+    return coef * np.log(x + 0.0001)
+
+if __name__ == "__main__":
+    df = pd.read_csv(sys.argv[1])
+    df = df.groupby(['tag', 'param', 'value']).\
+            agg({'time': 'mean'}).\
+            reset_index()
+
+    for (func, param), g in df.groupby(['tag', 'param']):
+        for model in [linear, square, log]:
+            popt, pcov = opt.curve_fit(model, g['value'], g['time'])
+            residuals = g['time'] - model(g['value'], *popt)
+            ss_res = np.sum(residuals**2)
+            ss_tot = np.sum((g['time']-np.mean(g['value']))**2)
+            r_squared = 1 - (ss_res / ss_tot)
+            print(model, r_squared)
