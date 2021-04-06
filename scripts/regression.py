ae4c9ec7d9c90851518bd331d3e2acc5a78e3f6e
Bruce Collie <brucecollie82@gmail.com>
2021-04-06 08:44:54 +0100

    Classify models

--- scripts/regression.py
+++ scripts/regression.py
@@ -5,17 +5,47 @@ import pandas as pd
 import scipy.optimize as opt
 import sys
 
-def const(x, cst):
-    return cst
+class Const:
+    def __repr__(self):
+        return "const"
 
-def linear(x, lin):
-    return lin * x
+    def __call__(self, x, cst):
+        return cst
 
-def square(x, coef):
-    return coef * (x ** 2)
+class Linear:
+    def __repr__(self):
+        return "linear"
 
-def log(x, coef):
-    return coef * np.log(x + 0.0001)
+    def __call__(self, x, lin):
+        return lin * x
+
+class Polynomial:
+    def __init__(self, exp):
+        self.exp = exp
+
+    def __repr__(self):
+        return f"poly({self.exp})"
+
+    def __call__(self, x, coef):
+        return coef * (x ** self.exp)
+
+class Log:
+    def __init__(self, base):
+        self.base = base
+
+    def __repr__(self):
+        return f"log({self.exp})"
+
+    def __call__(self, x, coef):
+        return coef * np.log(x + 0.0001)
+
+def fit_r_squared(model, xs, ys):
+    popt, pcov = opt.curve_fit(model, xs, ys)
+    residuals = ys - model(xs, *popt)
+    ss_res = np.sum(residuals**2)
+    ss_tot = np.sum((ys - np.mean(xs))**2)
+    r_squared = 1 - (ss_res / ss_tot)
+    return (popt, r_squared, model)
 
 if __name__ == "__main__":
     df = pd.read_csv(sys.argv[1])
@@ -24,10 +54,7 @@ if __name__ == "__main__":
             reset_index()
 
     for (func, param), g in df.groupby(['tag', 'param']):
-        for model in [const, linear, square, log]:
-            popt, pcov = opt.curve_fit(model, g['value'], g['time'])
-            residuals = g['time'] - model(g['value'], *popt)
-            ss_res = np.sum(residuals**2)
-            ss_tot = np.sum((g['time']-np.mean(g['value']))**2)
-            r_squared = 1 - (ss_res / ss_tot)
-            print(model, r_squared)
+        models = [Const(), Linear(), Polynomial(2), Log(2)]
+        scores = [fit_r_squared(m, g['value'], g['time']) for m in models]
+        best = max(scores, key=lambda s: s[1])
+        print(best)
