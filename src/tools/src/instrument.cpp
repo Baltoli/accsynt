9e24e9439a1e42c4add00e58e2028c8eaa90e147
Bruce Collie <brucecollie82@gmail.com>
2021-04-06 17:00:26 +0100

    Instrumentation boilerplate

--- src/tools/src/instrument.cpp
+++ src/tools/src/instrument.cpp
@@ -1,1 +1,60 @@
-int main() { }
+#include <support/file.h>
+#include <support/thread_context.h>
+
+#include <llvm/Bitcode/BitcodeWriter.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+
+#include <fmt/format.h>
+
+using namespace support;
+using namespace llvm;
+
+static cl::opt<std::string> InputFilename(
+    cl::Positional, cl::desc("<input bitcode file>"), cl::init("-"),
+    cl::value_desc("filename"));
+
+static cl::opt<std::string> OutputFilename(
+    "o", cl::desc("Filename to save the generated constraints to"),
+    cl::value_desc("filename"), cl::init("-"));
+
+static cl::opt<bool> Textual(
+    "S", cl::desc("Output module as textual IR instead of bitcode"),
+    cl::init(false));
+
+static cl::opt<bool> Force(
+    "f", cl::desc("Force binary output to the terminal"), cl::init(false));
+
+void instrument(Module& mod) { }
+
+int main(int argc, char** argv)
+{
+  cl::ParseCommandLineOptions(argc, argv);
+
+  auto& ctx = thread_context::get();
+  auto err = SMDiagnostic {};
+
+  auto&& mod = parseIRFile(InputFilename, err, ctx, true, "");
+  if (!mod) {
+    err.print(argv[0], errs());
+    return 1;
+  }
+
+  instrument(*mod);
+
+  to_file_or_default(OutputFilename, [&mod](auto&& os) {
+    if (!Textual) {
+      if (OutputFilename == "-" && !Force) {
+        fmt::print(
+            stderr, "Not outputting binary data to the terminal (use -f if "
+                    "you really want to)\n");
+        return;
+      }
+
+      WriteBitcodeToFile(*mod, os);
+    } else {
+      mod->print(os, nullptr);
+    }
+  });
+}
