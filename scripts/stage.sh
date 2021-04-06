872a58c859670ce4e95f04e53a5d230525ce3175
Bruce Collie <brucecollie82@gmail.com>
2021-04-06 10:18:29 +0100

    Implement staging script

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -1,11 +1,40 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 4 ]; then
+    echo "Usage: stage.sh model dir dylib output"
+    exit 1
+fi
+
+model="$1"
+shift
+
 dir="$1"
 shift
 
 dylib="$1"
 shift
 
-for f in $(find $dir -name '*.props'); do
-  echo $f
-done
+output="$1"
+shift
+
+file_names=$(find $dir -name '*.props')
+files=($file_names)
+i=1
+n=${#files[@]}
+
+script="get-perf-data.sh"
+
+echo "#!/usr/bin/env bash" > $script
+chmod +x $script
+
+for f in ${files[@]}; do
+  base=$(basename $f .props)
+  echo "echo \"[$i/$n] Running $base...\""
+  echo -n $model $f $dylib \
+    --linear --reps=10 \
+    --step=8 --start=0 --end=2048 \
+    --independent=512 --memsize=120
+  echo " > $output/$base.csv"
+
+  i=$((i+1))
+done > $script
