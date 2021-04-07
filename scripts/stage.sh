1f0d2e5cd6e125749facf2f278546fd40bee4359
Bruce Collie <brucecollie82@gmail.com>
2021-04-07 10:20:22 +0100

    Long, long run

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -31,8 +31,8 @@ for f in ${files[@]}; do
   base=$(basename $f .props)
   echo "echo \"[$i/$n] Running $base...\""
   echo -n $model $f $dylib \
-    --linear --reps=10 \
-    --step=4 --start=0 --end=4096 \
+    --linear --reps=30 \
+    --step=1 --start=0 --end=4096 \
     --independent=256 --memsize=140
   echo " > $output/$base.csv"
 
