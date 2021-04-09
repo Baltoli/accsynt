6c7320d682ae9095dc2101f2a006f03bcee2eec3
Bruce Collie <brucecollie82@gmail.com>
2021-04-09 09:54:26 +0100

    Rough structure of instrumentation visitor

--- src/tools/src/print_opcode.cpp
+++ src/tools/src/print_opcode.cpp
@@ -1,7 +1,24 @@
 #include "print_opcode.h"
 
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/Support/raw_ostream.h>
 
 using namespace llvm;
 
-void PrintOpcodeVisitor::visitInstruction(Instruction& inst) { }
+void PrintOpcodeVisitor::visitInstruction(Instruction& inst)
+{
+  auto mod = inst.getParent()->getParent()->getParent();
+  auto decl = printer_decl(*mod);
+
+  auto b = IRBuilder<>(&inst);
+
+  auto format = b.CreateGlobalStringPtr("%s\n");
+  auto str = b.CreateGlobalStringPtr("inst");
+
+  b.CreateCall(decl, {format, str});
+}
+
+Function* PrintOpcodeVisitor::printer_decl(Module& mod) const
+{
+  return nullptr;
+}
--- src/tools/src/print_opcode.h
+++ src/tools/src/print_opcode.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <llvm/IR/Function.h>
 #include <llvm/IR/InstVisitor.h>
 
 class PrintOpcodeVisitor : public llvm::InstVisitor<PrintOpcodeVisitor> {
 public:
   void visitInstruction(llvm::Instruction&);
+
+private:
+  llvm::Function* printer_decl(llvm::Module&) const;
 };
