3d23120098b958fec2e4b0f6d99493d45342c377
Bruce Collie <brucecollie82@gmail.com>
2021-04-12 10:39:42 +0100

    Add squashed instruction data

--- scripts/squash.py
+++ scripts/squash.py
@@ -4,8 +4,7 @@ import pandas as pd
 import sys
 
 if __name__ == "__main__":
-    df = pd.read_csv(sys.argv[1], names=['instruction', 'function'])
-    # df = df[df['function'] != 'disable_trace']
+    df = pd.read_csv(sys.argv[1], names=['instruction', 'function'], dtype={'instruction':'category'})
     df['freq'] = df.groupby('instruction')['instruction'].transform('count')
     df = df.drop_duplicates().reset_index(drop=True)
     print(df.to_csv(index=False))
