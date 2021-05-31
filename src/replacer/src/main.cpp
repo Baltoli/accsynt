913df648874b83af31a80ddda6b5a103990501d4
Bruce Collie <brucecollie82@gmail.com>
2021-05-31 11:03:02 +0100

    Start to sketch out replacer input

--- src/replacer/src/main.cpp
+++ src/replacer/src/main.cpp
@@ -5,6 +5,14 @@
 using namespace support;
 using namespace llvm;
 
+cl::opt<std::string> SpecFile(
+    cl::Positional, cl::desc("<spec file>"),
+    cl::value_desc("File containing replacement specification"), cl::Required);
+
+cl::opt<std::string> Bitcode(
+    cl::Positional, cl::desc("<bitcode file>"), cl::value_desc("Input bitcode"),
+    cl::Required);
+
 int main(int argc, char** argv)
 {
   hide_llvm_options();
